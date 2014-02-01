cb7938f4ba20db6206dfe9aeb88b3e579f37d39b
Jean-Luc Picard <jlp666@yandex.ru>
2014-02-01 13:06:19 +0200

    generate javadoc

--- compile.sh
+++ compile.sh
@@ -1,2 +1,7 @@
+CP=webapps/root/WEB-INF/lib/*:lib/*:lib/*/*
+SP=src/java/
+
 /bin/rm -rf webapps/root/WEB-INF/classes/*
-/usr/bin/javac -sourcepath src/java/ -classpath lib/*:lib/*/*:webapps/root/WEB-INF/lib/* -d webapps/root/WEB-INF/classes/ src/java/nxt/*.java src/java/nxt/*/*.java
+/bin/rm -rf webapps/root/doc/*
+/usr/bin/javac -sourcepath $SP -classpath $CP -d webapps/root/WEB-INF/classes/ src/java/nxt/*.java src/java/nxt/*/*.java
+/usr/bin/javadoc -sourcepath $SP -classpath $CP -protected -splitindex -subpackages nxt -d webapps/root/doc/
