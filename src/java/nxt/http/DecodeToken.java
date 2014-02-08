2cf0015fb54b64560cc82303649dd5724d2c5088
Jean-Luc Picard <jlp666@yandex.ru>
2014-02-08 15:28:23 +0200

    token processing

--- src/java/nxt/http/DecodeToken.java
+++ src/java/nxt/http/DecodeToken.java
@@ -1,15 +1,13 @@
 package nxt.http;
 
 import nxt.Account;
-import nxt.crypto.Crypto;
+import nxt.Token;
 import nxt.util.Convert;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONStreamAware;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.UnsupportedEncodingException;
 
-import static nxt.http.JSONResponses.INCORRECT_TOKEN;
 import static nxt.http.JSONResponses.INCORRECT_WEBSITE;
 import static nxt.http.JSONResponses.MISSING_TOKEN;
 import static nxt.http.JSONResponses.MISSING_WEBSITE;
@@ -24,56 +22,25 @@ public final class DecodeToken extends HttpRequestHandler {
     public JSONStreamAware processRequest(HttpServletRequest req) {
 
         String website = req.getParameter("website");
-        String token = req.getParameter("token");
+        String tokenString = req.getParameter("token");
         if (website == null) {
             return MISSING_WEBSITE;
-        } else if (token == null) {
+        } else if (tokenString == null) {
             return MISSING_TOKEN;
         }
 
         try {
-            byte[] websiteBytes = website.trim().getBytes("UTF-8");
-            byte[] tokenBytes = new byte[100];
-            int i = 0, j = 0;
-            try {
 
-                for (; i < token.length(); i += 8, j += 5) {
-
-                    long number = Long.parseLong(token.substring(i, i + 8), 32);
-                    tokenBytes[j] = (byte)number;
-                    tokenBytes[j + 1] = (byte)(number >> 8);
-                    tokenBytes[j + 2] = (byte)(number >> 16);
-                    tokenBytes[j + 3] = (byte)(number >> 24);
-                    tokenBytes[j + 4] = (byte)(number >> 32);
-
-                }
-
-            } catch (NumberFormatException e) {
-                return INCORRECT_TOKEN;
-            }
-
-            if (i != 160) {
-                return INCORRECT_TOKEN;
-            }
-            byte[] publicKey = new byte[32];
-            System.arraycopy(tokenBytes, 0, publicKey, 0, 32);
-            int timestamp = (tokenBytes[32] & 0xFF) | ((tokenBytes[33] & 0xFF) << 8) | ((tokenBytes[34] & 0xFF) << 16) | ((tokenBytes[35] & 0xFF) << 24);
-            byte[] signature = new byte[64];
-            System.arraycopy(tokenBytes, 36, signature, 0, 64);
-
-            byte[] data = new byte[websiteBytes.length + 36];
-            System.arraycopy(websiteBytes, 0, data, 0, websiteBytes.length);
-            System.arraycopy(tokenBytes, 0, data, websiteBytes.length, 36);
-            boolean valid = Crypto.verify(signature, data, publicKey);
+            Token token = Token.parseToken(tokenString, website.trim());
 
             JSONObject response = new JSONObject();
-            response.put("account", Convert.convert(Account.getId(publicKey)));
-            response.put("timestamp", timestamp);
-            response.put("valid", valid);
+            response.put("account", Convert.convert(Account.getId(token.getPublicKey())));
+            response.put("timestamp", token.getTimestamp());
+            response.put("valid", token.isValid());
 
             return response;
 
-        } catch (RuntimeException|UnsupportedEncodingException e) {
+        } catch (RuntimeException e) {
             return INCORRECT_WEBSITE;
         }
     }
--- src/java/nxt/user/GenerateAuthorizationToken.java
+++ src/java/nxt/user/GenerateAuthorizationToken.java
@@ -1,7 +1,6 @@
 package nxt.user;
 
-import nxt.crypto.Crypto;
-import nxt.util.Convert;
+import nxt.Token;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONStreamAware;
 
@@ -22,56 +21,8 @@ final class GenerateAuthorizationToken extends UserRequestHandler {
         if (! user.getSecretPhrase().equals(secretPhrase)) {
             return INVALID_SECRET_PHRASE;
         }
-        byte[] website = req.getParameter("website").trim().getBytes("UTF-8");
-        byte[] data = new byte[website.length + 32 + 4];
-        System.arraycopy(website, 0, data, 0, website.length);
-        System.arraycopy(user.getPublicKey(), 0, data, website.length, 32);
-        int timestamp = Convert.getEpochTime();
-        data[website.length + 32] = (byte)timestamp;
-        data[website.length + 32 + 1] = (byte)(timestamp >> 8);
-        data[website.length + 32 + 2] = (byte)(timestamp >> 16);
-        data[website.length + 32 + 3] = (byte)(timestamp >> 24);
 
-        byte[] token = new byte[100];
-        System.arraycopy(data, website.length, token, 0, 32 + 4);
-        System.arraycopy(Crypto.sign(data, user.getSecretPhrase()), 0, token, 32 + 4, 64);
-        String tokenString = "";
-        for (int ptr = 0; ptr < 100; ptr += 5) {
-
-            long number = ((long)(token[ptr] & 0xFF)) | (((long)(token[ptr + 1] & 0xFF)) << 8) | (((long)(token[ptr + 2] & 0xFF)) << 16)
-                    | (((long)(token[ptr + 3] & 0xFF)) << 24) | (((long)(token[ptr + 4] & 0xFF)) << 32);
-            if (number < 32) {
-
-                tokenString += "0000000";
-
-            } else if (number < 1024) {
-
-                tokenString += "000000";
-
-            } else if (number < 32768) {
-
-                tokenString += "00000";
-
-            } else if (number < 1048576) {
-
-                tokenString += "0000";
-
-            } else if (number < 33554432) {
-
-                tokenString += "000";
-
-            } else if (number < 1073741824) {
-
-                tokenString += "00";
-
-            } else if (number < 34359738368L) {
-
-                tokenString += "0";
-
-            }
-            tokenString += Long.toString(number, 32);
-
-        }
+        String tokenString = Token.generateToken(secretPhrase, req.getParameter("website").trim());
 
         JSONObject response = new JSONObject();
         response.put("response", "showAuthorizationToken");
