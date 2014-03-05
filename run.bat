c56a517ba5ae0957829131a57a189c01f9a7d3e8
Jean-Luc Picard <jlp666@yandex.ru>
2014-03-05 12:00:51 +0200

    updated run.bat

--- run.bat
+++ run.bat
@@ -1,13 +1,16 @@
-@ECHO OFF 
+@ECHO OFF
 IF EXIST java (
-    start java -cp nxt.jar;lib\*;conf nxt.Nxt
+	start "NXT NRS" java -cp nxt.jar;lib\*;conf nxt.Nxt
 ) ELSE (
-    IF EXIST "C:\Program Files\Java\jre7" (
-    start "C:\Program Files\Java\jre7\bin\java.exe" -cp nxt.jar;lib\*;conf nxt.Nxt
-    ) ELSE (
-        IF EXIST "C:\Program Files (x86)\Java\jre7" 
-        start "C:\Program Files (x86)\Java\jre7\bin\java.exe" -cp nxt.jar;lib\*;conf nxt.Nxt
-        ELSE ECHO Java software not found on your system. Please go to http://java.com/en/ to download a copy of Java.
-        PAUSE
-    )
+	IF EXIST "%PROGRAMFILES%\Java\jre7" (
+		start "NXT NRS" "%PROGRAMFILES%\Java\jre7\bin\java.exe" -cp nxt.jar;lib\*;conf nxt.Nxt
+	) ELSE (
+		IF EXIST "%PROGRAMFILES(X86)%\Java\jre7" (
+			start "NXT NRS" "%PROGRAMFILES(X86)%\Java\jre7\bin\java.exe" -cp nxt.jar;lib\*;conf nxt.Nxt
+		) ELSE (
+			ECHO Java software not found on your system. Please go to http://java.com/en/ to download a copy of Java.
+			PAUSE
+		)
+	)
 )
+
