c773e904068308672b346514f76571c3b167a8d7
Jean-Luc Picard <jlp666@yandex.ru>
2014-03-09 21:22:35 +0100

    prepare transaction bytes ready for signing if public key is supplied instead of secret phrase

--- src/java/nxt/http/TransferAsset.java
+++ src/java/nxt/http/TransferAsset.java
@@ -26,7 +26,7 @@ import static nxt.http.JSONResponses.MISSING_RECIPIENT;
 import static nxt.http.JSONResponses.MISSING_SECRET_PHRASE;
 import static nxt.http.JSONResponses.NOT_ENOUGH_FUNDS;
 
-public final class TransferAsset extends APIServlet.APIRequestHandler {
+public final class TransferAsset extends CreateTransaction {
 
     static final TransferAsset instance = new TransferAsset();
 
@@ -35,25 +35,16 @@ public final class TransferAsset extends APIServlet.APIRequestHandler {
     @Override
     JSONStreamAware processRequest(HttpServletRequest req) throws NxtException.ValidationException {
 
-        String secretPhrase = req.getParameter("secretPhrase");
         String recipientValue = req.getParameter("recipient");
         String assetValue = req.getParameter("asset");
         String quantityValue = req.getParameter("quantity");
-        String feeValue = req.getParameter("fee");
-        String deadlineValue = req.getParameter("deadline");
-        String referencedTransactionValue = req.getParameter("referencedTransaction");
-        if (secretPhrase == null) {
-            return MISSING_SECRET_PHRASE;
-        } else if (recipientValue == null || "0".equals(recipientValue)) {
+
+        if (recipientValue == null || "0".equals(recipientValue)) {
             return MISSING_RECIPIENT;
         } else if (assetValue == null) {
             return MISSING_ASSET;
         } else if (quantityValue == null) {
             return MISSING_QUANTITY;
-        } else if (feeValue == null) {
-            return MISSING_FEE;
-        } else if (deadlineValue == null) {
-            return MISSING_DEADLINE;
         }
 
         Long recipient;
@@ -80,37 +71,8 @@ public final class TransferAsset extends APIServlet.APIRequestHandler {
             return INCORRECT_QUANTITY;
         }
 
-        int fee;
-        try {
-            fee = Integer.parseInt(feeValue);
-            if (fee <= 0 || fee >= Nxt.MAX_BALANCE) {
-                return INCORRECT_FEE;
-            }
-        } catch (NumberFormatException e) {
-            return INCORRECT_FEE;
-        }
-
-        short deadline;
-        try {
-            deadline = Short.parseShort(deadlineValue);
-            if (deadline < 1 || deadline > 1440) {
-                return INCORRECT_DEADLINE;
-            }
-        } catch (NumberFormatException e) {
-            return INCORRECT_DEADLINE;
-        }
-
-        Long referencedTransaction;
-        try {
-            referencedTransaction = referencedTransactionValue == null ? null : Convert.parseUnsignedLong(referencedTransactionValue);
-        } catch (RuntimeException e) {
-            return INCORRECT_REFERENCED_TRANSACTION;
-        }
-
-        byte[] publicKey = Crypto.getPublicKey(secretPhrase);
-
-        Account account = Account.getAccount(publicKey);
-        if (account == null || fee * 100L > account.getUnconfirmedBalance()) {
+        Account account = getAccount(req);
+        if (account == null) {
             return NOT_ENOUGH_FUNDS;
         }
 
@@ -120,20 +82,8 @@ public final class TransferAsset extends APIServlet.APIRequestHandler {
         }
 
         Attachment attachment = new Attachment.ColoredCoinsAssetTransfer(asset, quantity);
-        Transaction transaction = Nxt.getTransactionProcessor().newTransaction(deadline, publicKey,
-                recipient, 0, fee, referencedTransaction, attachment);
-        transaction.sign(secretPhrase);
+        return createTransaction(req, account, recipient, 0, attachment);
 
-        Nxt.getTransactionProcessor().broadcast(transaction);
-
-        JSONObject response = new JSONObject();
-        response.put("transaction", transaction.getStringId());
-        return response;
-    }
-
-    @Override
-    boolean requirePost() {
-        return true;
     }
 
 }
