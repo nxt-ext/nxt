8d1ad8f6fd399e42d50e6dacef8febf5e1adc1e5
Jean-Luc Picard <jlp666@yandex.ru>
2014-03-14 01:09:29 +0100

    also return unconfirmed asset balances

--- src/java/nxt/http/GetAccount.java
+++ src/java/nxt/http/GetAccount.java
@@ -56,10 +56,22 @@ public final class GetAccount extends APIServlet.APIRequestHandler {
 
             }
             if (assetBalances.size() > 0) {
-
                 response.put("assetBalances", assetBalances);
+            }
+
+            JSONArray unconfirmedAssetBalances = new JSONArray();
+            for (Map.Entry<Long, Integer> unconfirmedAssetBalanceEntry : accountData.getUnconfirmedAssetBalances().entrySet()) {
+
+                JSONObject unconfirmedAssetBalance = new JSONObject();
+                unconfirmedAssetBalance.put("asset", Convert.toUnsignedLong(unconfirmedAssetBalanceEntry.getKey()));
+                unconfirmedAssetBalance.put("unconfirmedBalance", unconfirmedAssetBalanceEntry.getValue());
+                unconfirmedAssetBalances.add(unconfirmedAssetBalance);
 
             }
+            if (unconfirmedAssetBalances.size() > 0) {
+                response.put("unconfirmedAssetBalances", unconfirmedAssetBalances);
+            }
+
         }
         return response;
     }
