47516df2b377885fff36ff195f08cdab8daf9fdc
Jean-Luc Picard <jlp666@yandex.ru>
2014-03-28 10:15:03 +0100

    optionally filter unconfirmed transactions by account

--- src/java/nxt/http/GetUnconfirmedTransactionIds.java
+++ src/java/nxt/http/GetUnconfirmedTransactionIds.java
@@ -1,24 +1,47 @@
 package nxt.http;
 
+import nxt.Account;
 import nxt.Nxt;
 import nxt.Transaction;
+import nxt.util.Convert;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONStreamAware;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static nxt.http.JSONResponses.INCORRECT_ACCOUNT;
+import static nxt.http.JSONResponses.MISSING_ACCOUNT;
+import static nxt.http.JSONResponses.MISSING_TIMESTAMP;
+import static nxt.http.JSONResponses.UNKNOWN_ACCOUNT;
+
 public final class GetUnconfirmedTransactionIds extends APIServlet.APIRequestHandler {
 
     static final GetUnconfirmedTransactionIds instance = new GetUnconfirmedTransactionIds();
 
-    private GetUnconfirmedTransactionIds() {}
+    private GetUnconfirmedTransactionIds() {
+        super("account");
+    }
 
     @Override
     JSONStreamAware processRequest(HttpServletRequest req) {
 
+        String accountIdString = Convert.emptyToNull(req.getParameter("account"));
+        Long accountId = null;
+
+        if (accountIdString != null) {
+            try {
+                accountId = Convert.parseUnsignedLong(accountIdString);
+            } catch (RuntimeException e) {
+                return INCORRECT_ACCOUNT;
+            }
+        }
+
         JSONArray transactionIds = new JSONArray();
         for (Transaction transaction : Nxt.getTransactionProcessor().getAllUnconfirmedTransactions()) {
+            if (accountId != null && ! (accountId.equals(transaction.getSenderId()) || accountId.equals(transaction.getRecipientId()))) {
+                continue;
+            }
             transactionIds.add(transaction.getStringId());
         }
 
