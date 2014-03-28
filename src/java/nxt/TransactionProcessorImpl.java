eccfa60b1daa073a4a4335ac4e1f87e8abdb695e
Jean-Luc Picard <jlp666@yandex.ru>
2014-03-28 11:11:00 +0100

    better error handling

--- src/java/nxt/TransactionProcessorImpl.java
+++ src/java/nxt/TransactionProcessorImpl.java
@@ -395,7 +395,7 @@ final class TransactionProcessorImpl implements TransactionProcessor {
                 transactions.add(parseTransaction((JSONObject) transactionData));
             } catch (NxtException.ValidationException e) {
                 if (! (e instanceof TransactionType.NotYetEnabledException)) {
-                    Logger.logDebugMessage("Dropping invalid transaction: " + e.toString());
+                    Logger.logDebugMessage("Dropping invalid transaction: " + e.getMessage());
                 }
             }
         }
