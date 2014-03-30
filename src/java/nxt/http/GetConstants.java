e5df8c1c88770b196075e9abbe2b15515377fb3e
Come-from-Beyond <come-from-beyond@mail.ru>
2014-03-30 21:59:24 +0300

    Updated "getConstants" response.

--- src/java/nxt/http/GetConstants.java
+++ src/java/nxt/http/GetConstants.java
@@ -130,6 +130,16 @@ public final class GetConstants extends APIServlet.APIRequestHandler {
         subtypes.add(subtype);
         transactionType.put("subtypes", subtypes);
         transactionTypes.add(transactionType);
+        transactionType = new JSONObject();
+        transactionType.put("value", TransactionType.AccountControl.EFFECTIVE_BALANCE_LEASING.getType());
+        transactionType.put("description", "Account Control");
+        subtypes = new JSONArray();
+        subtype = new JSONObject();
+        subtype.put("value", TransactionType.AccountControl.EFFECTIVE_BALANCE_LEASING.getSubtype());
+        subtype.put("description", "Effective balance leasing");
+        subtypes.add(subtype);
+        transactionType.put("subtypes", subtypes);
+        transactionTypes.add(transactionType);
         response.put("transactionTypes", transactionTypes);
 
         JSONArray peerStates = new JSONArray();
