53baec098290a47eb071845722271b00967fb663
Jean-Luc Picard <jlp666@yandex.ru>
2014-04-09 13:06:39 +0200

    don't hardcode uncertain deadlines

--- src/java/nxt/Constants.java
+++ src/java/nxt/Constants.java
@@ -37,9 +37,9 @@ public final class Constants {
     public static final int TRANSPARENT_FORGING_BLOCK_3 = 51000;
     public static final int TRANSPARENT_FORGING_BLOCK_4 = 64000;
     public static final int TRANSPARENT_FORGING_BLOCK_5 = 67000;
-    public static final int NQT_BLOCK = isTestnet ? 63000 : 150000;
-    public static final int ASSET_EXCHANGE_BLOCK = isTestnet ? NQT_BLOCK : 211111;
-    public static final int VOTING_SYSTEM_BLOCK = isTestnet ? 0 : 222222;
+    public static final int NQT_BLOCK = isTestnet ? 63000 : Integer.MAX_VALUE;
+    public static final int ASSET_EXCHANGE_BLOCK = isTestnet ? NQT_BLOCK : Integer.MAX_VALUE;
+    public static final int VOTING_SYSTEM_BLOCK = isTestnet ? 0 : Integer.MAX_VALUE;
 
     public static final long EPOCH_BEGINNING;
     static {
