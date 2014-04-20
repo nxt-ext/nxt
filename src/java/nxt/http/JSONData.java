9f1a3508dc4788cab4f43f806f408a4156e18560
Jean-Luc Picard <jlp666@yandex.ru>
2014-04-21 00:03:52 +0200

    JSON still needs to include referencedTransactionId before NQT_BLOCK

--- src/java/nxt/http/JSONData.java
+++ src/java/nxt/http/JSONData.java
@@ -189,7 +189,7 @@ final class JSONData {
         json.put("recipient", Convert.toUnsignedLong(transaction.getRecipientId()));
         json.put("amountNQT", String.valueOf(transaction.getAmountNQT()));
         json.put("feeNQT", String.valueOf(transaction.getFeeNQT()));
-        json.put("referencedTransaction", Convert.toUnsignedLong(Convert.fullHashToId(transaction.getReferencedTransactionFullHash())));
+        json.put("referencedTransaction", Convert.toUnsignedLong(transaction.getReferencedTransactionId()));
         if (transaction.getReferencedTransactionFullHash() != null) {
             json.put("referencedTransactionFullHash", transaction.getReferencedTransactionFullHash());
         }
