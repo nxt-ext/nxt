bc5c67d28c74bc05d5d4578b230dacb97b7b638e
Wesley <wesleyh@telenet.be>
2014-04-23 17:40:07 +0200

    Set current account name/description when opening modal.

--- html/tools/js/nrs.modals.accountinfo.js
+++ html/tools/js/nrs.modals.accountinfo.js
@@ -1,4 +1,9 @@
 var NRS = (function(NRS, $, undefined) {
+	$("#account_info_modal").on("show.bs.modal", function(e) {
+		$("#account_info_name").val(NRS.accountInfo.name);
+		$("#account_info_description").val(NRS.accountInfo.description);
+	});
+
 	NRS.forms.setAccountInfoComplete = function(response, data) {
 		var name = $.trim(String(data.name));
 		if (name) {
