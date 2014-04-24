979df87815112c77c3672cf5fe38ad9d63da0bf0
Jean-Luc Picard <jlp666@yandex.ru>
2014-04-24 10:52:06 +0200

    also do verify

--- src/java/nxt/http/SignTransaction.java
+++ src/java/nxt/http/SignTransaction.java
@@ -49,6 +49,7 @@ public final class SignTransaction extends APIServlet.APIRequestHandler {
             response.put("fullHash", transaction.getFullHash());
             response.put("transactionBytes", Convert.toHexString(transaction.getBytes()));
             response.put("signatureHash", Convert.toHexString(Crypto.sha256().digest(transaction.getSignature())));
+            response.put("verify", transaction.verify());
             return response;
         } catch (NxtException.ValidationException|RuntimeException e) {
             //Logger.logDebugMessage(e.getMessage(), e);
