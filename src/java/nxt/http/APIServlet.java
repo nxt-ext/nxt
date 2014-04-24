416c7c268b5eb0391162cc488375d4bf845966eb
Jean-Luc Picard <jlp666@yandex.ru>
2014-04-24 10:44:58 +0200

    signTransaction API

--- src/java/nxt/http/APIServlet.java
+++ src/java/nxt/http/APIServlet.java
@@ -111,6 +111,7 @@ public final class APIServlet extends HttpServlet {
         map.put("sendMessage", SendMessage.instance);
         map.put("sendMoney", SendMoney.instance);
         map.put("setAccountInfo", SetAccountInfo.instance);
+        map.put("signTransaction", SignTransaction.instance);
         map.put("startForging", StartForging.instance);
         map.put("stopForging", StopForging.instance);
         map.put("getForging", GetForging.instance);
