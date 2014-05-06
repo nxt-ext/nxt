cd637a77949d9144e03f383cf1dcfbed57f8ca7c
Jean-Luc Picard <jlp666@yandex.ru>
2014-05-06 18:06:16 +0200

    getBlockchainStatus API request

---
+++ src/java/nxt/http/GetBlockchainStatus.java
@@ -0,0 +1,45 @@
+package nxt.http;
+
+import nxt.Account;
+import nxt.Alias;
+import nxt.Asset;
+import nxt.Block;
+import nxt.BlockchainProcessor;
+import nxt.Generator;
+import nxt.Nxt;
+import nxt.Order;
+import nxt.Poll;
+import nxt.Trade;
+import nxt.Vote;
+import nxt.peer.Peer;
+import nxt.peer.Peers;
+import nxt.util.Convert;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONStreamAware;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+public final class GetBlockchainStatus extends APIServlet.APIRequestHandler {
+
+    static final GetBlockchainStatus instance = new GetBlockchainStatus();
+
+    private GetBlockchainStatus() {}
+
+    @Override
+    JSONStreamAware processRequest(HttpServletRequest req) {
+        JSONObject response = new JSONObject();
+        response.put("time", Convert.getEpochTime());
+        Block lastBlock = Nxt.getBlockchain().getLastBlock();
+        response.put("lastBlock", lastBlock.getStringId());
+        response.put("cumulativeDifficulty", lastBlock.getCumulativeDifficulty().toString());
+        response.put("numberOfBlocks", lastBlock.getHeight() + 1);
+        BlockchainProcessor blockchainProcessor = Nxt.getBlockchainProcessor();
+        Peer lastBlockchainFeeder = blockchainProcessor.getLastBlockchainFeeder();
+        response.put("lastBlockchainFeeder", lastBlockchainFeeder == null ? null : lastBlockchainFeeder.getAnnouncedAddress());
+        response.put("lastBlockchainFeederHeight", blockchainProcessor.getLastBlockchainFeederHeight());
+        response.put("isScanning", blockchainProcessor.isScanning());
+        return response;
+    }
+
+}
