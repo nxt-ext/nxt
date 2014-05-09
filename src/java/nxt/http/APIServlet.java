f0ef25ca92edf804dcb141b8740c04a4b081f8cd
Jean-Luc Picard <jlp666@yandex.ru>
2014-05-10 00:55:47 +0200

    getUnconfirmedTransactions API

--- src/java/nxt/http/APIServlet.java
+++ src/java/nxt/http/APIServlet.java
@@ -98,6 +98,7 @@ public final class APIServlet extends HttpServlet {
         map.put("getTransaction", GetTransaction.instance);
         map.put("getTransactionBytes", GetTransactionBytes.instance);
         map.put("getUnconfirmedTransactionIds", GetUnconfirmedTransactionIds.instance);
+        map.put("getUnconfirmedTransactions", GetUnconfirmedTransactions.instance);
         map.put("getAccountCurrentAskOrderIds", GetAccountCurrentAskOrderIds.instance);
         map.put("getAccountCurrentBidOrderIds", GetAccountCurrentBidOrderIds.instance);
         map.put("getAllOpenOrders", GetAllOpenOrders.instance);
