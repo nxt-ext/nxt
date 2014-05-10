fc0314a7db7d78d6d50f89360a8c520b34c9e101
Wesley <wesleyh@telenet.be>
2014-05-10 20:55:45 +0200

    Minor.

--- html/ui/js/nrs.update.js
+++ html/ui/js/nrs.update.js
@@ -3,13 +3,14 @@ var NRS = (function(NRS, $, undefined) {
 	NRS.betaVersion = {};
 
 	NRS.checkAliasVersions = function() {
+		if (NRS.downloadingBlockchain) {
+			$("#nrs_update_explanation span").hide();
+			$("#nrs_update_explanation_blockchain_sync").show();
+			return;
+		}
 		if (NRS.isTestNet) {
 			$("#nrs_update_explanation span").hide();
 			$("#nrs_update_explanation_testnet").show();
-			return;
-		} else if (NRS.downloadingBlockchain) {
-			$("#nrs_update_explanation span").hide();
-			$("#nrs_update_explanation_blockchain_sync").show();
 			return;
 		}
 		//Get latest version nr+hash of normal version
