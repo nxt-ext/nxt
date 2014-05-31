7e09d1055c02dac4fc0cbac0fb81cce5cfdfbfc0
Jean-Luc Picard <jlp666@yandex.ru>
2014-05-31 13:35:00 -0700

    added rsConvert API request

---
+++ src/java/nxt/http/RSConvert.java
@@ -0,0 +1,40 @@
+package nxt.http;
+
+import nxt.util.Convert;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONStreamAware;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static nxt.http.JSONResponses.INCORRECT_ACCOUNT;
+import static nxt.http.JSONResponses.MISSING_ACCOUNT;
+
+public final class RSConvert extends APIServlet.APIRequestHandler {
+
+    static final RSConvert instance = new RSConvert();
+
+    private RSConvert() {
+        super("account");
+    }
+
+    @Override
+    JSONStreamAware processRequest(HttpServletRequest req) {
+        String accountValue = Convert.emptyToNull(req.getParameter("account"));
+        if (accountValue == null) {
+            return MISSING_ACCOUNT;
+        }
+        try {
+            Long accountId = Convert.parseAccountId(accountValue);
+            if (accountId == null) {
+                return INCORRECT_ACCOUNT;
+            }
+            JSONObject response = new JSONObject();
+            response.put("account", Convert.toUnsignedLong(accountId));
+            response.put("accountRS", Convert.rsAccount(accountId));
+            return response;
+        } catch (RuntimeException e) {
+            return INCORRECT_ACCOUNT;
+        }
+    }
+
+}
