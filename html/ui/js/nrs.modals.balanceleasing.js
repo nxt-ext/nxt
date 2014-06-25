87fae5a39bc7e074a6fbb565e7dbc833fc3b53a3
Wesley <wesleyh@telenet.be>
2014-06-25 14:14:29 +0200

    Lease balance period, show nr of days (avg).

--- html/ui/js/nrs.modals.balanceleasing.js
+++ html/ui/js/nrs.modals.balanceleasing.js
@@ -7,5 +7,18 @@ var NRS = (function(NRS, $, undefined) {
 		NRS.getAccountInfo();
 	}
 
+	$("#lease_balance_modal").on("show.bs.modal", function() {
+		$("#lease_balance_help").html("A lease of <strong>9000 blocks</strong> is about 10 days.");
+	});
+
+	$("#lease_balance_period").on("change", function() {
+		if ($(this).val() > 32767) {
+			$("#lease_balance_help").html("Lease period cannot be higher than <strong>32767 blocks</strong>.");
+		} else {
+			var days = Math.round($(this).val() / 900);
+			$("#lease_balance_help").html("A lease of <strong>" + $(this).val() + " blocks</strong> is about " + Math.round(days) + " days.");
+		}
+	});
+
 	return NRS;
 }(NRS || {}, jQuery));
