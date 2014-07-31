ac5c65d2131699510077f002da49414df3fff0fd
Wesley <wesleyh@telenet.be>
2014-07-31 22:12:46 +0200

    Remove nxtcrypto RSS feed.

--- html/ui/js/nrs.news.js
+++ html/ui/js/nrs.news.js
@@ -43,7 +43,6 @@ var NRS = (function(NRS, $, undefined) {
 			};
 
 			$("#nxtforum_news").rss("https://nxtforum.org/index.php?type=rss;action=.xml;sa=news;", settings, NRS.newsLoaded);
-			$("#nxtcrypto_news").rss("http://info.nxtcrypto.org/feed/", settings, NRS.newsLoaded);
 			$("#reddit_news").rss("http://www.reddit.com/r/NXT/.rss", settingsReddit, NRS.newsLoaded);
 			$("#nxtcoin_blogspot_news").rss("http://nxtcoin.blogspot.com/feeds/posts/default", settings, NRS.newsLoaded);
 			$("#nxter_news").rss("http://nxter.org/feed/", settings, NRS.newsLoaded);
