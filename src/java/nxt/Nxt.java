5662d70e57feb5be812eed3351d8700603fde6cf
Jean-Luc Picard <jlp666@yandex.ru>
2014-08-05 22:11:18 +0200

    changelog

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -16,7 +16,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "1.2.1e";
+    public static final String VERSION = "1.2.2";
     public static final String APPLICATION = "NRS";
 
     private static final Properties defaultProperties = new Properties();
