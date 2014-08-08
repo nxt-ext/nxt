c87a34ab7106f61ad59d36cc5a56a410177af4f6
Wesley <wesleyh@telenet.be>
2014-08-08 18:44:08 +0200

    Private IP range.

--- html/ui/js/nrs.util.js
+++ html/ui/js/nrs.util.js
@@ -676,7 +676,7 @@ var NRS = (function(NRS, $, undefined) {
 			return false;
 		}
 		var parts = ip.split('.');
-		if (parts[0] === '10' || (parts[0] === '172' && (parseInt(parts[1], 10) >= 16 && parseInt(parts[1], 10) <= 31)) || (parts[0] === '192' && parts[1] === '168')) {
+		if (parts[0] === '10' || parts[0] == '127' || (parts[0] === '172' && (parseInt(parts[1], 10) >= 16 && parseInt(parts[1], 10) <= 31)) || (parts[0] === '192' && parts[1] === '168')) {
 			return true;
 		}
 		return false;
