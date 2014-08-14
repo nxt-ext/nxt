e2f73760b24c683bb0a6c9a0db0fe8af0659a8d4
Jonny Heggheim <hegjon@gmail.com>
2014-08-14 20:13:12 +0200

    Added TokenTest

--- run-tests.sh
+++ run-tests.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 CP=conf/:classes/:lib/*:testlib/*
 SP=src/java/:test/java/
-TESTS="nxt.crypto.Curve25519Test nxt.crypto.ReedSolomonTest nxt.peer.HallmarkTest"
+TESTS="nxt.crypto.Curve25519Test nxt.crypto.ReedSolomonTest nxt.peer.HallmarkTest nxt.TokenTest"
 
 /bin/mkdir -p classes/
 
-javac -sourcepath $SP -classpath $CP -d classes/ src/java/nxt/*.java src/java/nxt/*/*.java test/java/nxt/*/*.java || exit 1
+javac -sourcepath $SP -classpath $CP -d classes/ src/java/nxt/*.java src/java/nxt/*/*.java test/java/nxt/*.java test/java/nxt/*/*.java || exit 1
 
 java -classpath $CP org.junit.runner.JUnitCore $TESTS
 
