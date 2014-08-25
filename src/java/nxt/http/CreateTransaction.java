d21f65b5e1daf5258e95d7e707c07588da4b1bff
Jean-Luc Picard <jlp666@yandex.ru>
2014-08-25 11:19:04 +0200

    removed "comment" parameter

--- src/java/nxt/http/CreateTransaction.java
+++ src/java/nxt/http/CreateTransaction.java
@@ -84,12 +84,6 @@ abstract class CreateTransaction extends APIServlet.APIRequestHandler {
             } catch (RuntimeException e) {
                 throw new ParameterException(INCORRECT_ARBITRARY_MESSAGE);
             }
-        } else if (attachment instanceof Attachment.ColoredCoinsAssetTransfer) {
-            // TODO: remove after DGS block
-            String commentValue = Convert.emptyToNull(req.getParameter("comment"));
-            if (commentValue != null) {
-                message = new Appendix.Message(commentValue);
-            }
         }
         Appendix.PublicKeyAnnouncement publicKeyAnnouncement = null;
         String recipientPublicKey = Convert.emptyToNull(req.getParameter("recipientPublicKey"));
