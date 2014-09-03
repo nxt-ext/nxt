cc78e6e065894afdc8c29e0087d1e5c7791cb128
lyaffe <lyaffe@gmail.com>
2014-09-03 19:39:04 +0300

    Created currency and account_currency tables. Initial work on account balances Removed the SuperComplexNumber Issuer account

--- src/java/nxt/Currency.java
+++ src/java/nxt/Currency.java
@@ -1,9 +1,16 @@
 package nxt;
 
+import nxt.db.DbIterator;
+import nxt.db.DbKey;
+import nxt.db.EntityDbTable;
 import nxt.util.Convert;
 import nxt.util.Listener;
 import org.eclipse.jetty.util.ConcurrentHashSet;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -13,6 +20,7 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+@SuppressWarnings("UnusedDeclaration")
 public final class Currency {
 
     public static final class NonIssuedCurrency {
@@ -42,6 +50,54 @@ public final class Currency {
             }
         }
 
+    }
+
+    private static final DbKey.LongKeyFactory<Currency> currencyDbKeyFactory = new DbKey.LongKeyFactory<Currency>("id") {
+
+        @Override
+        public DbKey newKey(Currency currency) {
+            return currency.dbKey;
+        }
+
+    };
+
+    private static final EntityDbTable<Currency> currencyTable = new EntityDbTable<Currency>(currencyDbKeyFactory) {
+
+        @Override
+        protected String table() {
+            return "asset";
+        }
+
+        @Override
+        protected Currency load(Connection con, ResultSet rs) throws SQLException {
+            return new Currency(rs);
+        }
+
+        @Override
+        protected void save(Connection con, Currency currency) throws SQLException {
+            currency.save(con);
+        }
+
+    };
+
+    public static DbIterator<Currency> getAllCurrencies(int from, int to) {
+        return currencyTable.getAll(from, to);
+    }
+
+    public static int getCount() {
+        return currencyTable.getCount();
+    }
+
+    public static Currency getCurrency(Long id) {
+        return currencyTable.get(currencyDbKeyFactory.newKey(id));
+    }
+
+    public static DbIterator<Currency> getCurrencyIssuedBy(Long accountId, int from, int to) {
+        return currencyTable.getManyBy("account_id", accountId, from, to);
+    }
+
+    static void addCurrency(Transaction transaction, Attachment.MonetarySystemCurrencyIssuance attachment) {
+        currencyTable.insert(new Currency(transaction, attachment));
     }
 
     private static final ConcurrentMap<Long, Currency> currencies = new ConcurrentHashMap<>();
@@ -83,16 +139,12 @@ public final class Currency {
         return allCurrencies;
     }
 
-    public static Currency getCurrency(Long id) {
-        return currencies.get(id);
-    }
-
     static void addNXTCurrency() {
         addCurrency(0L, "Nxt", "NXT", "", (byte)0, Constants.MAX_BALANCE_NQT, 0, 1, (byte)0, (byte)0, (byte)0, Constants.MAX_BALANCE_NQT, 0);
     }
 
     static void addCurrency(Long currencyId, String name, String code, String description, byte type, long totalSupply, int issuanceHeight, long minReservePerUnitNQT, byte minDifficulty, byte maxDifficulty, byte ruleset, long currentSupply, long currentReservePerUnitNQT) {
-        Currency currency = new Currency(currencyId, name, code, description, type, totalSupply, issuanceHeight, minReservePerUnitNQT, minDifficulty, maxDifficulty, ruleset, currentSupply, currentReservePerUnitNQT);
+        Currency currency = new Currency(currencyId, (long)-1, name, code, description, type, totalSupply, issuanceHeight, minReservePerUnitNQT, minDifficulty, maxDifficulty, ruleset, currentSupply, currentReservePerUnitNQT);
         if (Currency.currencies.putIfAbsent(currencyId, currency) != null) {
             throw new IllegalStateException("Currency with id " + Convert.toUnsignedLong(currencyId) + " already exists");
         }
@@ -120,6 +172,9 @@ public final class Currency {
     }
 
     private final Long currencyId;
+
+    private final DbKey dbKey;
+    private final Long accountId;
     private final String name;
     private final String code;
     private final String description;
@@ -130,12 +185,20 @@ public final class Currency {
     private final byte minDifficulty;
     private final byte maxDifficulty;
     private final byte ruleset;
-
     private long currentSupply;
+
     private long currentReservePerUnitNQT;
 
-    private Currency(Long currencyId, String name, String code, String description, byte type, long totalSupply, int issuanceHeight, long minReservePerUnitNQT, byte minDifficulty, byte maxDifficulty, byte ruleset, long currentSupply, long currentReservePerUnitNQT) {
+    public Currency(Transaction transaction, Attachment.MonetarySystemCurrencyIssuance attachment) {
+        this(transaction.getId(), (long)0, attachment.getName(), attachment.getCode(), attachment.getDescription(), attachment.getType(),
+                attachment.getTotalSupply(), attachment.getIssuanceHeight(), attachment.getMinReservePerUnitNQT(),
+                attachment.getMinDifficulty(), attachment.getMaxDifficulty(), attachment.getRuleset(), attachment.getTotalSupply(), 0);
+    }
+
+    private Currency(Long currencyId, Long accountId, String name, String code, String description, byte type, long totalSupply, int issuanceHeight, long minReservePerUnitNQT, byte minDifficulty, byte maxDifficulty, byte ruleset, long currentSupply, long currentReservePerUnitNQT) {
         this.currencyId = currencyId;
+        this.dbKey = currencyDbKeyFactory.newKey(this.currencyId);
+        this.accountId = accountId;
         this.name = name;
         this.code = code;
         this.description = description;
@@ -146,13 +209,58 @@ public final class Currency {
         this.minDifficulty = minDifficulty;
         this.maxDifficulty = maxDifficulty;
         this.ruleset = ruleset;
-        
         this.currentSupply = currentSupply;
         this.currentReservePerUnitNQT = currentReservePerUnitNQT;
     }
 
+    private Currency(ResultSet rs) throws SQLException {
+        this.currencyId = rs.getLong("id");
+        this.dbKey = currencyDbKeyFactory.newKey(this.currencyId);
+        this.accountId = rs.getLong("account_id");
+        this.name = rs.getString("name");
+        this.code = rs.getString("code");
+        this.description = rs.getString("description");
+        this.type = rs.getByte("type");
+        this.totalSupply = rs.getLong("total_supply");
+        this.issuanceHeight = rs.getInt("issuance_height");
+        this.minReservePerUnitNQT = rs.getLong("min_reserve_per_unit_nqt");
+        this.minDifficulty = rs.getByte("min_difficulty");
+        this.maxDifficulty = rs.getByte("max_difficulty");
+        this.ruleset = rs.getByte("ruleset");
+        this.currentSupply = rs.getLong("current_supply");
+        this.currentReservePerUnitNQT = rs.getLong("current_reserve_per_unit_nqt");
+    }
+
+    private void save(Connection con) throws SQLException {
+        try (PreparedStatement pstmt = con.prepareStatement("INSERT INTO currency (id, account_id, name, code, "
+                + "description, type, total_supply, issuance_height, min_reserve_per_unit_nqt, "
+                + "min_difficulty, max_difficulty, ruleset, current_supply, current_reserve_per_unit_nqt) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+            int i = 0;
+            pstmt.setLong(++i, this.getCurrencyId());
+            pstmt.setLong(++i, this.getAccountId());
+            pstmt.setString(++i, this.getName());
+            pstmt.setString(++i, this.getCode());
+            pstmt.setString(++i, this.getDescription());
+            pstmt.setByte(++i, this.getType());
+            pstmt.setLong(++i, this.getTotalSupply());
+            pstmt.setInt(++i, this.getIssuanceHeight());
+            pstmt.setLong(++i, this.getMinReservePerUnitNQT());
+            pstmt.setByte(++i, this.getMinDifficulty());
+            pstmt.setByte(++i, this.getMaxDifficulty());
+            pstmt.setByte(++i, this.getRuleset());
+            pstmt.setLong(++i, this.getCurrentSupply());
+            pstmt.setLong(++i, this.getCurrentReservePerUnitNQT());
+            pstmt.executeUpdate();
+        }
+    }
+
     public Long getCurrencyId() {
         return currencyId;
+    }
+
+    public Long getAccountId() {
+        return accountId;
     }
 
     public String getName() {
