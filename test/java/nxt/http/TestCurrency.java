acf1662989b9f404c3b90d7f4a3a97074a2dfffb
lyaffe <lyaffe@gmail.com>
2014-09-14 12:54:18 +0300

    Unit test prototype

--- test/java/nxt/http/TestCurrency.java
+++ test/java/nxt/http/TestCurrency.java
@@ -1,18 +1,98 @@
 package nxt.http;
 
-import nxt.Attachment;
+import nxt.Block;
+import nxt.BlockchainProcessor;
+import nxt.Generator;
+import nxt.Nxt;
+import nxt.util.Listener;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class TestCurrency extends CreateTransaction {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestCurrency {
 
     @Test
     public void issueSimpleCurrency() {
-        Attachment attachment = new Attachment.MonetarySystemCurrencyIssuance("TestCur", "TST", "Test Currency", 1, 1000,
-                0, 0, 0, 0, 0);
+        Nxt.init();
+        Map<String, String> reqParams = new HashMap<>();
+        reqParams.put("requestType", "issueCurrency");
+        String secretPhrase = "hope peace happen touch easy pretend worthless talk them indeed wheel state";
+        reqParams.put("secretPhrase", secretPhrase);
+        reqParams.put("deadline", "1440");
+        reqParams.put("feeNQT", "100000000000");
+        reqParams.put("name", "Test1");
+        reqParams.put("code", "TSX");
+        reqParams.put("description", "Test Currency 1");
+        reqParams.put("type", "1");
+        reqParams.put("totalSupply", "100000");
 
-        return new CreateTransaction().createTransaction(req, account, attachment);
+        JSONObject issueCurrencyResponse = processRequest(reqParams);
+        String currencyId = (String) issueCurrencyResponse.get("transaction");
+        System.out.println("issueCurrencyResponse: " + issueCurrencyResponse.toJSONString());
+        Generator.startForging(secretPhrase);
+        Generator.stopForging(secretPhrase);
 
+        reqParams.clear();
+        reqParams.put("requestType", "getCurrency");
+        reqParams.put("currency", currencyId);
+        JSONObject getCurrencyResponse = processRequest(reqParams);
+        System.out.println("getCurrencyResponse:" + getCurrencyResponse.toJSONString());
+        Assert.assertEquals(currencyId, getCurrencyResponse.get("currency"));
+        Assert.assertEquals("TSX", getCurrencyResponse.get("code"));
     }
 
+    @After
+    public void destroy() {
+        Map<String, String> reqParams = new HashMap<>();
+        reqParams.put("requestType", "popOff");
+        reqParams.put("numBlocks", "1");
+        JSONObject popOffResponse = processRequest(reqParams);
+        System.out.println("popOffResponse:" + popOffResponse.toJSONString());
+    }
 
+    private JSONObject processRequest(Map<String, String> reqParams) {
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        when(req.getRemoteHost()).thenReturn("localhost");
+        when(req.getMethod()).thenReturn("POST");
+        for (String key : reqParams.keySet()) {
+            when(req.getParameter(key)).thenReturn(reqParams.get(key));
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintWriter writer = new PrintWriter(new OutputStreamWriter(out));
+        try {
+            when(resp.getWriter()).thenReturn(writer);
+            APIServlet apiServlet = new APIServlet();
+            apiServlet.doPost(req, resp);
+        } catch (ServletException | IOException e) {
+            Assert.fail();
+        }
+        return (JSONObject) JSONValue.parse(new InputStreamReader(new ByteArrayInputStream(out.toByteArray())));
+    }
+
+    @Before
+    public void init() {
+
+        Nxt.getBlockchainProcessor().addListener(new BlockListener(), BlockchainProcessor.Event.BLOCK_GENERATED);
+    }
+
+    private static class BlockListener implements Listener<Block> {
+        @Override
+        public void notify(Block block) {
+            System.out.printf("Block Generated at height %d with %d transactions\n", block.getHeight(), block.getTransactionIds().size());
+        }
+    }
 }
