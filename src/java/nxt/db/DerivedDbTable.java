5d6503d859d46aa4674c7e84c0f58782c1e8c373
Jean-Luc Picard <jlp666@yandex.ru>
2014-10-08 23:59:37 +0200

    derived tables don't have referential constraints anyway

--- src/java/nxt/db/DerivedDbTable.java
+++ src/java/nxt/db/DerivedDbTable.java
@@ -35,9 +35,7 @@ public abstract class DerivedDbTable {
         }
         try (Connection con = Db.getConnection();
              Statement stmt = con.createStatement()) {
-            stmt.executeUpdate("SET REFERENTIAL_INTEGRITY FALSE");
             stmt.executeUpdate("TRUNCATE TABLE " + table);
-            stmt.executeUpdate("SET REFERENTIAL_INTEGRITY TRUE");
         } catch (SQLException e) {
             throw new RuntimeException(e.toString(), e);
         }
