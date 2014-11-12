abf3b5b3c5b4834d78002cd8da94086a5c850bff
lyaffe <lyaffe@gmail.com>
2014-11-12 11:04:41 +0200

    Unit tests

--- test/java/nxt/http/SendMoneyTest.java
+++ test/java/nxt/http/SendMoneyTest.java
@@ -91,6 +91,78 @@ public class SendMoneyTest extends BlockchainTest {
         Assert.assertEquals(-Constants.ONE_NXT, testers.get(2).getUnconfirmedBalanceDiff());
     }
 
+    @Test
+    public void signAndBroadcastBytes() {
+        JSONObject response = new APICall.Builder("sendMoney").
+                param("publicKey", testers.get(1).getPublicKeyStr()).
+                param("recipient", testers.get(2).getStrId()).
+                param("amountNQT", 100 * Constants.ONE_NXT).
+                param("feeNQT", Constants.ONE_NXT).
+                build().invoke();
+        Logger.logDebugMessage("sendMoney: " + response);
+        generateBlock();
+        // No change transaction not broadcast
+        Assert.assertEquals(0, testers.get(1).getBalanceDiff());
+        Assert.assertEquals(0, testers.get(1).getUnconfirmedBalanceDiff());
+        Assert.assertEquals(0, testers.get(2).getBalanceDiff());
+        Assert.assertEquals(0, testers.get(2).getUnconfirmedBalanceDiff());
+
+        response = new APICall.Builder("signTransaction").
+                param("secretPhrase", testers.get(1).getSecretPhrase()).
+                param("unsignedTransactionBytes", (String)response.get("unsignedTransactionBytes")).
+                build().invoke();
+        Logger.logDebugMessage("signTransaction: " + response);
+
+        response = new APICall.Builder("broadcastTransaction").
+                param("transactionBytes", (String)response.get("transactionBytes")).
+                build().invoke();
+        Logger.logDebugMessage("broadcastTransaction: " + response);
+        generateBlock();
+
+        // Sender
+        Assert.assertEquals(-100 * Constants.ONE_NXT - Constants.ONE_NXT, testers.get(1).getBalanceDiff());
+        Assert.assertEquals(-100 * Constants.ONE_NXT - Constants.ONE_NXT, testers.get(1).getUnconfirmedBalanceDiff());
+        // Recipient
+        Assert.assertEquals(100 * Constants.ONE_NXT, testers.get(2).getBalanceDiff());
+        Assert.assertEquals(100 * Constants.ONE_NXT, testers.get(2).getUnconfirmedBalanceDiff());
+    }
+
+    @Test
+    public void signAndBroadcastJSON() {
+        JSONObject response = new APICall.Builder("sendMoney").
+                param("publicKey", testers.get(1).getPublicKeyStr()).
+                param("recipient", testers.get(2).getStrId()).
+                param("amountNQT", 100 * Constants.ONE_NXT).
+                param("feeNQT", Constants.ONE_NXT).
+                build().invoke();
+        Logger.logDebugMessage("sendMoney: " + response);
+        generateBlock();
+        // No change transaction not broadcast
+        Assert.assertEquals(0, testers.get(1).getBalanceDiff());
+        Assert.assertEquals(0, testers.get(1).getUnconfirmedBalanceDiff());
+        Assert.assertEquals(0, testers.get(2).getBalanceDiff());
+        Assert.assertEquals(0, testers.get(2).getUnconfirmedBalanceDiff());
+
+        response = new APICall.Builder("signTransaction").
+                param("secretPhrase", testers.get(1).getSecretPhrase()).
+                param("unsignedTransactionJSON", response.get("transactionJSON").toString()).
+                build().invoke();
+        Logger.logDebugMessage("signTransaction: " + response);
+
+        response = new APICall.Builder("broadcastTransaction").
+                param("transactionBytes", (String)response.get("transactionBytes")).
+                build().invoke();
+        Logger.logDebugMessage("broadcastTransaction: " + response);
+        generateBlock();
+
+        // Sender
+        Assert.assertEquals(-100 * Constants.ONE_NXT - Constants.ONE_NXT, testers.get(1).getBalanceDiff());
+        Assert.assertEquals(-100 * Constants.ONE_NXT - Constants.ONE_NXT, testers.get(1).getUnconfirmedBalanceDiff());
+        // Recipient
+        Assert.assertEquals(100 * Constants.ONE_NXT, testers.get(2).getBalanceDiff());
+        Assert.assertEquals(100 * Constants.ONE_NXT, testers.get(2).getUnconfirmedBalanceDiff());
+    }
+
     @BeforeClass
     public static void beforeClass() {
         Nxt.init();
