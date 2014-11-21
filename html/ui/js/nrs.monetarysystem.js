76f54ad7587b1601f9fdf7d3ca1ca8c9e498f6c0
MrV <mrv777@comcast.net>
2014-11-21 13:21:22 -0600

    Work on automatic buy/sell buttons in MS

--- html/ui/js/nrs.monetarysystem.js
+++ html/ui/js/nrs.monetarysystem.js
@@ -165,24 +165,25 @@ var NRS = (function(NRS, $, undefined) {
 	
 	/* Monetary System Auto Quantity TODO */
 	$("#sell_automatic_currency_price, #buy_automatic_currency_price").on("click", function(e) {
-		/*try {
+		try {
 			
-			var type = ($(this).attr("id") == "sell_automatic_price" ? "sell" : "buy");
-
+			var type = ($(this).attr("id") == "sell_automatic_currency_price" ? "sell" : "buy");
+			var currencyBalance = parseFloat($("#your_currency_balance").html().replace("'",""));
+			
 			var price = new Big(NRS.convertToNQT(String($("#" + type + "_currency_price").val())));
-			var balance = new Big(type == "buy" ? NRS.accountInfo.unconfirmedBalanceNQT : $("#your_currency_balance").html());
+			var balance = new Big(type == "buy" ? NRS.accountInfo.unconfirmedBalanceNQT : currencyBalance);
 			var balanceNQT = new Big(NRS.accountInfo.unconfirmedBalanceNQT);
-			var maxQuantity = new Big(NRS.convertToQNTf($("#your_currency_balance").html(), $("#currency_decimals").html()));
+			var maxQuantity = new Big(NRS.convertToQNT(currencyBalance, $("#currency_decimals").html()));
 
 			if (balance.cmp(new Big("0")) <= 0) {
 				return;
 			}
 
-			if (price.cmp(new Big("0")) <= 0) {
+			/*if (price.cmp(new Big("0")) <= 0) {
 				//get minimum price if no offers exist, based on currency decimals..
 				price = new Big("" + Math.pow(10, $("#currency_decimals").html()));
 				$("#" + type + "_currency_price").val(NRS.convertToNXT(price.toString()));
-			}
+			}*/
 
 			var quantity = new Big(NRS.amountToPrecision((type == "sell" ? balanceNQT : balance).div(price).toString(), $("#currency_decimals").html()));
 
@@ -209,7 +210,7 @@ var NRS = (function(NRS, $, undefined) {
 				"background": "",
 				"color": ""
 			});
-		} catch (err) {}*/
+		} catch (err) {}
 	});
 	
 	/* Monetary System Page Search capitalization */
@@ -520,8 +521,8 @@ var NRS = (function(NRS, $, undefined) {
 				for (var i = 0; i < response.exchanges.length; i++) {
                 var exchange = response.exchanges[i];
 					rows += "<tr><td><a href='#' data-transaction='" + String(exchange.transaction).escapeHTML() + "'>" + String(exchange.transaction).escapeHTML() + "</a></td>" +
-					"<td>" + (exchange.sellerRS == NRS.accountRS ? "You" : exchange.sellerRS) + "</td>" +
-					"<td>" + (exchange.buyerRS == NRS.accountRS ? "You" : exchange.buyerRS) + "</td>" +
+					"<td>" + (exchange.sellerRS == NRS.accountRS ? "You" : "<a href='#' data-user='" + String(exchange.sellerRS).escapeHTML() + "'>" + String(exchange.sellerRS).escapeHTML() + "</a>") + "</td>" +
+					"<td>" + (exchange.buyerRS == NRS.accountRS ? "You" : "<a href='#' data-user='" + String(exchange.buyerRS).escapeHTML() + "'>" + String(exchange.buyerRS).escapeHTML() + "</a>") + "</td>" +
                   	"<td>" + exchange.name + "</td>" +
                   	"<td>" + exchange.code + "</td>" +
                   	"<td>" + NRS.formatQuantity(exchange.units, exchange.decimals) + "</td>" +
