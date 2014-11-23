c576d993b6ac73b86fede8fba2d5971b43233bcd
Lizyaffe <lyaffe@gmail.com>
2014-11-23 23:17:06 +0200

    Documentation

--- build-exe.bat
+++ build-exe.bat
@@ -1,5 +1,5 @@
-rem @echo off
+@echo off
 pushd dist
 del /q setup.exe
-..\installerlib\izpack2exe\izpack2exe.py --file setup.jar --output setup.exe --with-7z=..\installerlib\izpack2exe\7za.exe --no-upx --with-jdk=..\jre --name nxt
+..\installerlib\izpack2exe\izpack2exe.py --file setup.jar --output setup.exe --with-7z=..\installerlib\izpack2exe\7za.exe --no-upx --with-jdk=..\jre --name nxt > build-exe.log 2>&1
 popd
