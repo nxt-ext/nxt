f11a6018db64f269aaa67c6ced7be27c6b9482d6
holgerd77 <Holger.Drewes@googlemail.com>
2014-11-24 17:52:52 +0100

    Added Nxt adress auto-formatting, +/- account ui structure, help icon to 2-phased tx UI modal

--- html/ui/js/nrs.modals.approve.js
+++ html/ui/js/nrs.modals.approve.js
@@ -13,6 +13,19 @@ var NRS = (function(NRS, $, undefined) {
         _currentModal.find('.tx-modal-approve a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
             $(e.target).closest('.tx-modal-approve').find('.tab-pane input').prop('disabled', true);
             $(e.target).closest('.tx-modal-approve').find('.tab-pane.active input').prop('disabled', false);
+
+            var $inputFields = $(e.target).closest('.tx-modal-approve').find("input[name=pendingWhitelisted], input[name=pendingBlacklisted]").not("[type=hidden]");
+
+            $.each($inputFields, function() {
+                if ($(this).hasClass("noMask")) {
+                    $(this).mask("NXT-****-****-****-*****", {
+                        "noMask": true
+                    }).removeClass("noMask");
+                } else {
+                    $(this).mask("NXT-****-****-****-*****");
+                }
+            });
+
         });
     }
 
