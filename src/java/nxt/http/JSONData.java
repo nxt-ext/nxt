e18f30cbf98f213c9ab47c361b6e01d47e622f70
lyaffe <lyaffe@gmail.com>
2014-12-24 23:31:28 +0200

    Add issuer account to putCurrencyIinfo

--- src/java/nxt/http/JSONData.java
+++ src/java/nxt/http/JSONData.java
@@ -514,6 +514,7 @@ final class JSONData {
         json.put("type", currency.getType());
         json.put("decimals", currency.getDecimals());
         json.put("issuanceHeight", currency.getIssuanceHeight());
+        putAccount(json, "issuerAccount", currency.getAccountId());
     }
 
     static void putAssetInfo(JSONObject json, long assetId) {
