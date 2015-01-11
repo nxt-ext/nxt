dc21665439c060fb87ce63a929ce2d883ff7dc49
lyaffe <lyaffe@gmail.com>
2015-01-11 12:01:30 +0200

    Fix the usage of contacts in transfer currency dialog

--- html/ui/js/nrs.recipient.js
+++ html/ui/js/nrs.recipient.js
@@ -3,7 +3,7 @@
  */
 var NRS = (function(NRS, $, undefined) {
 	NRS.automaticallyCheckRecipient = function() {
-		var $recipientFields = $("#send_money_recipient, #transfer_asset_recipient, #send_message_recipient, #add_contact_account_id, #update_contact_account_id, #lease_balance_recipient, #transfer_alias_recipient, #sell_alias_recipient");
+		var $recipientFields = $("#send_money_recipient, #transfer_asset_recipient, #transfer_currency_recipient, #send_message_recipient, #add_contact_account_id, #update_contact_account_id, #lease_balance_recipient, #transfer_alias_recipient, #sell_alias_recipient");
 
 		$recipientFields.on("blur", function() {
 			$(this).trigger("checkRecipient");
