da7649170ee3c225bceb9b75c2d81a60b425788d
Jean-Luc Picard <jlp666@yandex.ru>
2015-01-22 15:52:40 +0100

    limit total number of known peers

--- src/java/nxt/peer/Peers.java
+++ src/java/nxt/peer/Peers.java
@@ -76,6 +76,8 @@ public final class Peers {
     private static final String myHallmark;
     private static final boolean shareMyAddress;
     private static final int maxNumberOfConnectedPublicPeers;
+    private static final int maxNumberOfKnownPeers;
+    private static final int minNumberOfKnownPeers;
     private static final boolean enableHallmarkProtection;
     private static final int pushThreshold;
     private static final int pullThreshold;
@@ -175,6 +177,8 @@ public final class Peers {
         }
 
         maxNumberOfConnectedPublicPeers = Nxt.getIntProperty("nxt.maxNumberOfConnectedPublicPeers");
+        maxNumberOfKnownPeers = Nxt.getIntProperty("nxt.maxNumberOfKnownPeers");
+        minNumberOfKnownPeers = Nxt.getIntProperty("nxt.minNumberOfKnownPeers");
         connectTimeout = Nxt.getIntProperty("nxt.connectTimeout");
         readTimeout = Nxt.getIntProperty("nxt.readTimeout");
         enableHallmarkProtection = Nxt.getBooleanProperty("nxt.enableHallmarkProtection");
@@ -351,6 +355,19 @@ public final class Peers {
                         }
                     }
 
+                    if (hasTooManyKnownPeers() && hasEnoughConnectedPublicPeers(Peers.maxNumberOfConnectedPublicPeers)) {
+                        Logger.logDebugMessage("Peer pool size is " + peers.size() + ", removing known peers last updated more than a week ago");
+                        for (PeerImpl peer : peers.values()) {
+                            if (now - peer.getLastUpdated() > 7 * 24 * 3600) {
+                                peer.remove();
+                            }
+                            if (hasTooFewKnownPeers()) {
+                                break;
+                            }
+                        }
+                        Logger.logDebugMessage("Peer pool size is " + peers.size());
+                    }
+
                 } catch (Exception e) {
                     Logger.logDebugMessage("Error connecting to peer", e);
                 }
@@ -388,7 +405,9 @@ public final class Peers {
 
             try {
                 try {
-
+                    if (hasTooManyKnownPeers()) {
+                        return;
+                    }
                     Peer peer = getAnyPeer(Peer.State.CONNECTED, true);
                     if (peer == null) {
                         return;
@@ -654,11 +673,13 @@ public final class Peers {
             Logger.logDebugMessage("Peer " + peerAddress + " is using testnet port " + peer.getPort() + ", ignoring");
             return null;
         }
-        peers.put(peerAddress, peer);
-        if (announcedAddress != null) {
-            updateAddress(peer);
+        if (!hasTooManyKnownPeers()) {
+            peers.put(peerAddress, peer);
+            if (announcedAddress != null) {
+                updateAddress(peer);
+            }
+            listeners.notify(peer, Event.NEW_PEER);
         }
-        listeners.notify(peer, Event.NEW_PEER);
         return peer;
     }
 
@@ -679,6 +700,7 @@ public final class Peers {
                 Peers.notifyListeners(oldPeer, Peers.Event.REMOVE);
             }
         }
+        peers.put(peer.getPeerAddress(), peer);
     }
 
     public static void connectPeer(Peer peer) {
@@ -812,6 +834,14 @@ public final class Peers {
         } catch (URISyntaxException |UnknownHostException e) {
             return null;
         }
+    }
+
+    static boolean hasTooFewKnownPeers() {
+        return peers.size() < Peers.minNumberOfKnownPeers;
+    }
+
+    static boolean hasTooManyKnownPeers() {
+        return peers.size() > Peers.maxNumberOfKnownPeers;
     }
 
     private static boolean hasEnoughConnectedPublicPeers(int limit) {
