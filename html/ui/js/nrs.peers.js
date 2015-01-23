f72bb4f0d898271e2bedba9e0616b390a5bc0a96
petkop <air_biker@abv.bg>
2015-01-23 02:55:12 +0200

    UI for adding and blacklisting peers

--- html/ui/js/nrs.peers.js
+++ html/ui/js/nrs.peers.js
@@ -36,7 +36,8 @@ var NRS = (function(NRS, $, undefined) {
 					}
 
 					rows += "<tr><td>" + (peer.state == 1 ? "<i class='fa fa-check-circle' style='color:#5cb85c' title='Connected'></i>" : "<i class='fa fa-times-circle' style='color:#f0ad4e' title='Disconnected'></i>") + "&nbsp;&nbsp;" + (peer.announcedAddress ? String(peer.announcedAddress).escapeHTML() : "No name") + "</td><td" + (peer.weight > 0 ? " style='font-weight:bold'" : "") + ">" + NRS.formatWeight(peer.weight) + "</td><td>" + NRS.formatVolume(peer.downloadedVolume) + "</td><td>" + NRS.formatVolume(peer.uploadedVolume) + "</td><td><span class='label label-" +
-						(NRS.versionCompare(peer.version, versionToCompare) >= 0 ? "success" : "danger") + "'>" + (peer.application && peer.version ? String(peer.application).escapeHTML() + " " + String(peer.version).escapeHTML() : "?") + "</label></td><td>" + (peer.platform ? String(peer.platform).escapeHTML() : "?") + "</td></tr>";
+						(NRS.versionCompare(peer.version, versionToCompare) >= 0 ? "success" : "danger") + "'>" + (peer.application && peer.version ? String(peer.application).escapeHTML() + " " + String(peer.version).escapeHTML() : "?") + "</label></td><td>" + (peer.platform ? String(peer.platform).escapeHTML() : "?") + "</td>" + 
+						"<td><a class='btn btn-xs btn-default' href='#' data-toggle='modal' data-target='#blacklist_peer_modal' data-peer='" + String(peer.announcedAddress).escapeHTML() + "'>" + $.t("blacklist") + "</a></td></tr>";
 				}
 
 				$("#peers_uploaded_volume").html(NRS.formatVolume(uploaded)).removeClass("loading_dots");
@@ -55,6 +56,29 @@ var NRS = (function(NRS, $, undefined) {
 	NRS.incoming.peers = function() {
 		NRS.loadPage("peers");
 	}
+	
+	NRS.forms.addPeerComplete = function(response, data) {
+		$.growl($.t("success_add_peer"), {
+			"type": "success"
+		});
+		NRS.loadPage("peers");
+	}
+	
+	NRS.forms.blacklistPeerComplete = function(response, data) {
+		$.growl($.t("success_blacklist_peer"), {
+			"type": "success"
+		});
+		NRS.loadPage("peers");
+	}
+	
+	$("#blacklist_peer_modal").on("show.bs.modal", function(e) {
+		var $invoker = $(e.relatedTarget);
 
+		var peerAddress = $invoker.data("peer");
+		
+		$("#blacklist_peer_address").html(peerAddress);
+		$("#blacklist_peer_field_id").val(peerAddress);
+	});
+	
 	return NRS;
 }(NRS || {}, jQuery));
