740bf95776c8a9b9a4ddb6d6bcfb277eb43a365b
Jean-Luc Picard <jlp666@yandex.ru>
2015-02-12 12:49:38 +0100

    changelog

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -17,7 +17,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "1.4.14";
+    public static final String VERSION = "1.4.15";
     public static final String APPLICATION = "NRS";
 
     private static volatile Time time = new Time.EpochTime();
