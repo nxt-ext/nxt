80de078fc48a88eaac761e368179d3e5c09d4c25
holgerd77 <Holger.Drewes@googlemail.com>
2015-02-17 18:22:40 +0100

    Added blockchain status loading as exemplaric functionality for hello_world demo plugin

--- html/ui/plugins/hello_world/js/nrs.hello_world.js
+++ html/ui/plugins/hello_world/js/nrs.hello_world.js
@@ -2,10 +2,22 @@
  * @depends {nrs.js}
  */
 var NRS = (function(NRS, $, undefined) {
-	
+
 	NRS.pages.plugin_hello_world = function() {
-		NRS.pageLoaded();
+		var rows = "";
+		
+		NRS.sendRequest("getBlockchainStatus", {}, function(response) {
+			if (response.lastBlock != undefined) {
+				$.each(response, function(fieldName, value) {
+					rows += "<tr><td>" + String(fieldName).escapeHTML() + "</td><td>" + String(value).escapeHTML() + "</td></tr>"; 
+				});
+			}
+			NRS.dataLoaded(rows);
+		});
 	}
 
 	return NRS;
 }(NRS || {}, jQuery));
+
+//File name for debugging (Chrome/Firefox)
+//@ sourceURL=nrs.hello_world.js
