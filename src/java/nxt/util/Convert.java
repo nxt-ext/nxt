f7929d6d3c8301da8a201f31397813930acd4704
kushti <kushtech@yahoo.com>
2015-02-24 00:39:36 +0300

    parseAccountId - check for isEmpty

--- src/java/nxt/util/Convert.java
+++ src/java/nxt/util/Convert.java
@@ -88,7 +88,7 @@ public final class Convert {
     }
 
     public static long parseAccountId(String account) {
-        if (account == null) {
+        if (account == null || account.isEmpty()) {
             return 0;
         }
         account = account.toUpperCase();
