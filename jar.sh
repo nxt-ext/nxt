31c1242bf4fd8af400d182224ba9e7929acd5823
Lizyaffe <lyaffe@gmail.com>
2015-02-26 00:03:18 +0200

    And update the Unix script

--- jar.sh
+++ jar.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 java -cp classes nxt.util.ManifestGenerator
 /bin/rm -f nxt.jar
-jar cfm nxt.jar src/META-INF/MANIFEST.MF -C classes . || exit 1
+jar cfm nxt.jar resource/nxt.manifest.mf -C classes . || exit 1
 /bin/rm -f nxtservice.jar
-jar cfm nxtservice.jar src/META-INF/MANIFEST.service.MF -C classes . || exit 1
+jar cfm nxtservice.jar resource/nxtservice.manifest.mf -C classes . || exit 1
 
 echo "jar files generated successfully"
