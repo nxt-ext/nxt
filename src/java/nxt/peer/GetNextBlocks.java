3e3886a41ce6fb67ae04e86ffcfe228d50b9b7cb
Jean-Luc Picard <jlp666@yandex.ru>
2015-03-11 01:48:12 +0100

    use of streams not needed in getNextBlocks

--- src/java/nxt/peer/GetNextBlocks.java
+++ src/java/nxt/peer/GetNextBlocks.java
@@ -8,7 +8,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONStreamAware;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 final class GetNextBlocks extends PeerServlet.PeerRequestHandler {
 
@@ -21,11 +20,15 @@ final class GetNextBlocks extends PeerServlet.PeerRequestHandler {
     JSONStreamAware processRequest(JSONObject request, Peer peer) {
 
         JSONObject response = new JSONObject();
+        JSONArray nextBlocksArray = new JSONArray();
 
         long blockId = Convert.parseUnsignedLong((String) request.get("blockId"));
         List<? extends Block> blocks = Nxt.getBlockchain().getBlocksAfter(blockId, 720);
 
-        response.put("nextBlocks", blocks.stream().map(Block::getJSONObject).collect(Collectors.toCollection(JSONArray::new)));
+        for (Block block : blocks) {
+            nextBlocksArray.add(block.getJSONObject());
+        }
+        response.put("nextBlocks", nextBlocksArray);
 
         return response;
     }
