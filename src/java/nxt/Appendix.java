823b09c65218282f3fd0daf22b41248a49137b31
Jean-Luc Picard <jlp666@yandex.ru>
2015-04-10 02:25:09 +0300

    fix message download from old peers

--- src/java/nxt/Appendix.java
+++ src/java/nxt/Appendix.java
@@ -130,7 +130,7 @@ public interface Appendix {
     class Message extends AbstractAppendix {
 
         static Message parse(JSONObject attachmentData) {
-            if (!hasAppendix("Message", attachmentData)) {
+            if (!hasAppendix("Message", attachmentData) && attachmentData.get("message") == null) { //TODO: VOTING_SYSTEM_BLOCK
                 return null;
             }
             return new Message(attachmentData);
