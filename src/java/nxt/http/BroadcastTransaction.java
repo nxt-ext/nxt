ffe322f9eb34acbe68e5286e3e3d535c8b00995a
lyaffe <lyaffe@gmail.com>
2015-04-15 23:27:40 +0300

    JavaDoc

--- src/java/nxt/http/BroadcastTransaction.java
+++ src/java/nxt/http/BroadcastTransaction.java
@@ -10,6 +10,26 @@ import org.json.simple.JSONStreamAware;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * The purpose of broadcast transaction is to support client side signing of transactions.
+ * Clients first submit their transaction using {@link nxt.http.CreateTransaction} without providing the secret phrase.<br>
+ * In response the client receives the unsigned transaction JSON and transaction bytes.
+ * <p>
+ * The client then signs and submits the signed transaction using {@link nxt.http.BroadcastTransaction}
+ * <p>
+ * The default wallet implements this procedure in nrs.server.js which you can use as reference.
+ * <p>
+ * {@link nxt.http.BroadcastTransaction} accepts the following parameters:<br>
+ * transactionJSON - JSON representation of the signed transaction<br>
+ * transactionBytes - row bytes composing the signed transaction bytes excluding the prunable appendages<br>
+ * prunableAttachmentJSON - JSON representation of the prunable appendages<br>
+ * <p>
+ * Clients can submit either the signed transactionJSON or the signed transactionBytes but not both.<br>
+ * In case the client submits transactionBytes for a transaction containing prunable appendages, the client also needs
+ * to submit the prunableAttachmentJSON parameter which includes the attachment JSON for the prunable appendages.<br>
+ * <p>
+ * Prunable appendages are classes implementing the {@link nxt.Appendix.Prunable} interface.
+ */
 public final class BroadcastTransaction extends APIServlet.APIRequestHandler {
 
     static final BroadcastTransaction instance = new BroadcastTransaction();
