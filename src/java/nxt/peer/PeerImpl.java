0585fca677b4df6b002fa83842b863b8aad4fc36
Jean-Luc Picard <jlp666@yandex.ru>
2015-04-26 22:13:31 +0300

    minor

--- src/java/nxt/peer/PeerImpl.java
+++ src/java/nxt/peer/PeerImpl.java
@@ -438,7 +438,7 @@ final class PeerImpl implements Peer {
                 URI uri = new URI("http://" + announcedAddress);
                 InetAddress inetAddress = InetAddress.getByName(uri.getHost());
                 if (!inetAddress.equals(InetAddress.getByName(host))) {
-                    Logger.logDebugMessage("Announced address " + announcedAddress + " now points to " + inetAddress.getHostAddress() + ", replacing peer");
+                    Logger.logDebugMessage("Announced address " + announcedAddress + " now points to " + inetAddress.getHostAddress() + ", replacing peer " + host);
                     Peers.removePeer(this);
                     PeerImpl newPeer = Peers.findOrCreatePeer(inetAddress, announcedAddress, true);
                     if (newPeer != null) {
