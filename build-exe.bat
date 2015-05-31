0636322ca794739350da9e14c32b414e9dede534
lyaffe <lyaffe@gmail.com>
2015-05-31 12:44:08 +0300

    Build the exe in the current folder

--- build-exe.bat
+++ build-exe.bat
@@ -1,5 +1,3 @@
 @echo off
-pushd dist
-del /q setup.exe
-..\installerlib\izpack2exe\izpack2exe.py --file setup.jar --output setup.exe --with-7z=..\installerlib\izpack2exe\7za.exe --no-upx --with-jdk=..\jre --name nxt > ..\build-exe.log 2>&1
-popd
+del /q %1.exe
+installerlib\izpack2exe\izpack2exe.py --file %1.jar --output %1.exe --with-7z=installerlib\izpack2exe\7za.exe --no-upx --with-jdk=jre --name nxt > build-exe.log 2>&1
