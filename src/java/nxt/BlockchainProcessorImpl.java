da8912248f3591013e5f84c982235c7d41d75814
Jean-Luc Picard <jlp666@yandex.ru>
2015-07-05 17:36:28 +0200

    also call analyze tables in a separate thread

--- src/java/nxt/BlockchainProcessorImpl.java
+++ src/java/nxt/BlockchainProcessorImpl.java
@@ -844,7 +844,9 @@ final class BlockchainProcessorImpl implements BlockchainProcessor {
             if (block.getHeight() % 5000 == 0) {
                 Logger.logMessage("received block " + block.getHeight());
                 if (!isDownloading || block.getHeight() % 50000 == 0) {
-                    Db.db.analyzeTables();
+                    networkService.submit(() -> {
+                        Db.db.analyzeTables();
+                    });
                 }
             }
         }, Event.BLOCK_PUSHED);
