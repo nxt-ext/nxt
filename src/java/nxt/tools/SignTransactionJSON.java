6607d52d1836563b07aab3318684a7af4a138cd3
Jean-Luc Picard <jlp666@yandex.ru>
2015-08-13 21:45:41 +0300

    added sign.sh script for command line transaction json signing

--- src/java/nxt/tools/SignTransactionJSON.java
+++ src/java/nxt/tools/SignTransactionJSON.java
@@ -18,7 +18,9 @@ package nxt.tools;
 
 import nxt.Nxt;
 import nxt.Transaction;
+import nxt.crypto.Crypto;
 import nxt.util.Convert;
+import nxt.util.Logger;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
@@ -34,6 +36,7 @@ public final class SignTransactionJSON {
 
     public static void main(String[] args) {
         try {
+            Logger.setLevel(Logger.Level.ERROR);
             if (args.length != 2) {
                 System.out.println("Usage: SignTransactionJSON <unsigned transaction json file> <signed transaction json file>");
                 System.exit(1);
@@ -48,22 +51,25 @@ public final class SignTransactionJSON {
                 System.out.println("File already exists: " + signed.getAbsolutePath());
                 System.exit(1);
             }
-            String secretPhrase;
-            Console console = System.console();
-            if (console == null) {
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-                    secretPhrase = reader.readLine();
-                }
-            } else {
-                secretPhrase = new String(console.readPassword("Secret phrase: "));
-            }
             try (BufferedReader reader = new BufferedReader(new FileReader(unsigned));
                  BufferedWriter writer = new BufferedWriter(new FileWriter(signed))) {
                 JSONObject json = (JSONObject) JSONValue.parseWithException(reader);
+                byte[] publicKeyHash = Crypto.sha256().digest(Convert.parseHexString((String) json.get("senderPublicKey")));
+                String senderRS = Convert.rsAccount(Convert.fullHashToId(publicKeyHash));
+                String secretPhrase;
+                Console console = System.console();
+                if (console == null) {
+                    try (BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in))) {
+                        secretPhrase = inputReader.readLine();
+                    }
+                } else {
+                    secretPhrase = new String(console.readPassword("Secret phrase for account " + senderRS + ": "));
+                }
                 Transaction.Builder builder = Nxt.newTransactionBuilder(json);
                 Transaction transaction = builder.build(secretPhrase);
                 writer.write(transaction.getJSONObject().toJSONString());
                 writer.newLine();
+                System.out.println("Signed transaction JSON saved as: " + signed.getAbsolutePath());
             }
         } catch (Exception e) {
             e.printStackTrace();
