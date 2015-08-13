1e1eef815bd3e59477aad4e4b6159ea792278c23
lyaffe <lyaffe@gmail.com>
2015-08-13 10:53:34 +0300

    Make sure block info dialog does not hang in case of exception

--- html/ui/js/nrs.modals.block.js
+++ html/ui/js/nrs.modals.block.js
@@ -41,36 +41,37 @@ var NRS = (function(NRS, $, undefined) {
 	});
 
 	NRS.showBlockModal = function(block) {
-		$("#block_info_modal_block").html(String(block.block).escapeHTML());
-		$("#block_info_transactions_tab_link").tab("show");
+        try {
+            $("#block_info_modal_block").html(String(block.block).escapeHTML());
+            $("#block_info_transactions_tab_link").tab("show");
 
-		var blockDetails = $.extend({}, block);
-		delete blockDetails.transactions;
-		delete blockDetails.previousBlockHash;
-		delete blockDetails.nextBlockHash;
-		delete blockDetails.generationSignature;
-		delete blockDetails.payloadHash;
-		delete blockDetails.block;
-		if (blockDetails.timestamp) {
-            blockDetails.blockGenerationTime = NRS.formatTimestamp(blockDetails.timestamp);
-        }
-		var detailsTable = $("#block_info_details_table");
-		detailsTable.find("tbody").empty().append(NRS.createInfoTable(blockDetails));
-		detailsTable.show();
-		var transactionsTable = $("#block_info_transactions_table");
-        if (block.transactions.length) {
-			$("#block_info_transactions_none").hide();
-			transactionsTable.show();
-			block.transactions.sort(function(a, b) {
-				return a.timestamp - b.timestamp;
-			});
-			var rows = "";
-			for (var i = 0; i < block.transactions.length; i++) {
-				var transaction = block.transactions[i];
-				if (transaction.amountNQT) {
-					transaction.amount = new BigInteger(transaction.amountNQT);
-					transaction.fee = new BigInteger(transaction.feeNQT);
-                    rows += "<tr>" +
+            var blockDetails = $.extend({}, block);
+            delete blockDetails.transactions;
+            delete blockDetails.previousBlockHash;
+            delete blockDetails.nextBlockHash;
+            delete blockDetails.generationSignature;
+            delete blockDetails.payloadHash;
+            delete blockDetails.block;
+            if (blockDetails.timestamp) {
+                blockDetails.blockGenerationTime = NRS.formatTimestamp(blockDetails.timestamp);
+            }
+            var detailsTable = $("#block_info_details_table");
+            detailsTable.find("tbody").empty().append(NRS.createInfoTable(blockDetails));
+            detailsTable.show();
+            var transactionsTable = $("#block_info_transactions_table");
+            if (block.transactions.length) {
+                $("#block_info_transactions_none").hide();
+                transactionsTable.show();
+                block.transactions.sort(function (a, b) {
+                    return a.timestamp - b.timestamp;
+                });
+                var rows = "";
+                for (var i = 0; i < block.transactions.length; i++) {
+                    var transaction = block.transactions[i];
+                    if (transaction.amountNQT) {
+                        transaction.amount = new BigInteger(transaction.amountNQT);
+                        transaction.fee = new BigInteger(transaction.feeNQT);
+                        rows += "<tr>" +
                         "<td><a href='#' class='show_transaction_modal_action' data-transaction='" + String(transaction.transaction).escapeHTML() + "'>" + NRS.formatTimestamp(transaction.timestamp) + "</a></td>" +
                         "<td>" + NRS.getTransactionIconHTML(transaction.type, transaction.subtype) + "</td>" +
                         "<td>" + NRS.formatAmount(transaction.amount) + "</td>" +
@@ -78,17 +79,17 @@ var NRS = (function(NRS, $, undefined) {
                         "<td>" + NRS.getAccountTitle(transaction, "recipient") + "</td>" +
                         "<td>" + NRS.getAccountTitle(transaction, "sender") + "</td>" +
                         "</tr>";
+                    }
                 }
-			}
-			transactionsTable.find("tbody").empty().append(rows);
-			$("#block_info_modal").modal("show");
-			NRS.fetchingModalData = false;
-		} else {
-			$("#block_info_transactions_none").show();
-			transactionsTable.hide();
-			$("#block_info_modal").modal("show");
-			NRS.fetchingModalData = false;
-		}
+                transactionsTable.find("tbody").empty().append(rows);
+            } else {
+                $("#block_info_transactions_none").show();
+                transactionsTable.hide();
+            }
+            $("#block_info_modal").modal("show");
+        } finally {
+            NRS.fetchingModalData = false;
+        }
 	};
 
 	return NRS;
