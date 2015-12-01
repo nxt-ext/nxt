2ae641279ed9c131f9c265021add390b2af99f75
lyaffe <lyaffe@gmail.com>
2015-12-01 23:44:50 +0200

    Add registrant count column to the shuffling table and account info modal

--- html/ui/js/nrs.modals.transaction.js
+++ html/ui/js/nrs.modals.transaction.js
@@ -1175,6 +1175,7 @@ var NRS = (function (NRS, $, undefined) {
                     "type": $.t("shuffling_creation"),
                     "period": transaction.attachment.registrationPeriod,
                     "participants": transaction.attachment.participantCount,
+                    "registrants": transaction.attachment.registeredCount,
                     "holdingType": transaction.attachment.holdingType
                 };
                 if (transaction.attachment.holding != "0") {
