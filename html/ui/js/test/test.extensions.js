7e2a894ac5ebd6c5705f12769eb9d4637fea3728
lyaffe <lyaffe@gmail.com>
2015-12-23 09:53:19 +0200

    Unit tests for extensions.js

---
+++ html/ui/js/test/test.extensions.js
@@ -0,0 +1,15 @@
+QUnit.module("extensions");
+
+QUnit.test("escapeHtml", function (assert) {
+    assert.equal("&\"<>/'".escapeHTML(), "&amp;&quot;&lt;&gt;&#x2F;&#39;", "all.escaped.chars");
+    assert.equal("<script>alert('hi');</script>".escapeHTML(), "&lt;script&gt;alert(&#39;hi&#39;);&lt;&#x2F;script&gt;", "xss.prevention");
+});
+
+QUnit.test("autoLink", function (assert) {
+    assert.equal("<script>alert('hi');</script>".autoLink(), "&lt;script&gt;alert(&#39;hi&#39;);&lt;/script&gt;", "xss.prevention");
+    assert.equal("Click http://nxt.org".autoLink(), "Click <a href='http://nxt.org' target='_blank'>http://nxt.org</a>", "simple.link");
+    assert.equal("https://www.google.co.il/?gfe_rd=cr&ei=X1J6Vu-QBceF8Qe6wKAY&gws_rd=ssl#q=%D7%A9%D7%9C%D7%95%D7%9D".autoLink(),
+        "<a href='https://www.google.co.il/?gfe_rd=cr&amp;ei=X1J6Vu-QBceF8Qe6wKAY&amp;gws_rd=ssl#q=%D7%A9%D7%9C%D7%95%D7%9D' target='_blank'>https://www.google.co.il/?gfe_rd=cr&amp;ei=X1J6Vu-QBceF8Qe6wKAY&amp;gws_rd=ssl#q=%D7%A9%D7%9C%D7%95%D7%9D</a>",
+        "complex.link");
+});
+
