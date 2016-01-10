b8081a67673d596accf9a0c5dfdbb0f47acee949
lyaffe <lyaffe@gmail.com>
2016-01-10 23:11:07 +0200

    Hide header buttons for disabledAPIs

---
+++ html/ui/js/nrs.header.js
@@ -0,0 +1,37 @@
+/******************************************************************************
+ * Copyright © 2013-2016 The Nxt Core Developers.                             *
+ *                                                                            *
+ * See the AUTHORS.txt, DEVELOPER-AGREEMENT.txt and LICENSE.txt files at      *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * Nxt software, including this file, may be copied, modified, propagated,    *
+ * or distributed except according to the terms contained in the LICENSE.txt  *
+ * file.                                                                      *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+/**
+ * @depends {nrs.js}
+ */
+var NRS = (function(NRS, $) {
+
+    function widgetVisibility(widget, depends) {
+        if (NRS.isApiEnabled(depends)) {
+            widget.show();
+        } else {
+            widget.hide();
+        }
+    }
+
+    $(window).load(function() {
+        widgetVisibility($("#header_send_money"), { apis: [NRS.constants.REQUEST_TYPES.sendMoney] });
+        widgetVisibility($("#header_transfer_currency"), { apis: [NRS.constants.REQUEST_TYPES.transferCurrency] });
+        widgetVisibility($("#header_send_message"), { apis: [NRS.constants.REQUEST_TYPES.sendMessage] });
+    });
+
+    return NRS;
+}(NRS || {}, jQuery));
