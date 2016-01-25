9088dc0b86c3d34347de95c9609679dfad2defbc
Jean-Luc Picard <jlp@mailbox.org>
2016-01-25 12:18:32 +0200

    updated initial peers

--- src/java/nxt/Constants.java
+++ src/java/nxt/Constants.java
@@ -149,7 +149,7 @@ public final class Constants {
     public static final int CHECKSUM_BLOCK_17 = isTestnet ? 484000 : 622000;
 
     public static final int LAST_CHECKSUM_BLOCK = CHECKSUM_BLOCK_17;
-    public static final int LAST_KNOWN_BLOCK = isTestnet ? 553000 : 622000;
+    public static final int LAST_KNOWN_BLOCK = isTestnet ? 555000 : 623000;
 
     public static final int[] MIN_VERSION = new int[] {1, 7};
 
