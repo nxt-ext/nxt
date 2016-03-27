213e5ed34a4c697808e63366ab8022a94afe83a4
Jean-Luc Picard <jlp@mailbox.org>
2016-03-27 14:54:07 +0300

    support downloading of encrypted part, if secretPhrase is supplied

--- src/java/nxt/http/DownloadPrunableMessage.java
+++ src/java/nxt/http/DownloadPrunableMessage.java
@@ -16,10 +16,13 @@
 
 package nxt.http;
 
+import nxt.Account;
 import nxt.Nxt;
 import nxt.NxtException;
 import nxt.PrunableMessage;
-import nxt.util.JSON;
+import nxt.crypto.Crypto;
+import nxt.crypto.EncryptedData;
+import nxt.util.Convert;
 import org.json.simple.JSONStreamAware;
 
 import javax.servlet.http.HttpServletRequest;
@@ -34,7 +37,7 @@ public final class DownloadPrunableMessage extends APIServlet.APIRequestHandler 
     static final DownloadPrunableMessage instance = new DownloadPrunableMessage();
 
     private DownloadPrunableMessage() {
-        super(new APITag[] {APITag.MESSAGES}, "transaction", "retrieve");
+        super(new APITag[] {APITag.MESSAGES}, "transaction", "secretPhrase", "retrieve");
     }
 
     @Override
@@ -48,10 +51,31 @@ public final class DownloadPrunableMessage extends APIServlet.APIRequestHandler 
             }
             prunableMessage = PrunableMessage.getPrunableMessage(transactionId);
         }
-        if (prunableMessage == null || prunableMessage.getMessage() == null) {
-            return JSON.emptyJSON;
+        String secretPhrase = ParameterParser.getSecretPhrase(request, false);
+        byte[] data = null;
+        if (prunableMessage != null) {
+            if (secretPhrase != null) {
+                EncryptedData encryptedData = prunableMessage.getEncryptedData();
+                if (encryptedData != null) {
+                    byte[] publicKey = prunableMessage.getSenderId() == Account.getId(Crypto.getPublicKey(secretPhrase))
+                            ? Account.getPublicKey(prunableMessage.getRecipientId()) : Account.getPublicKey(prunableMessage.getSenderId());
+                    if (publicKey != null) {
+                        try {
+                            data = Account.decryptFrom(publicKey, encryptedData, secretPhrase, prunableMessage.isCompressed());
+                        } catch (RuntimeException e) {
+                            return JSONResponses.error("Decryption failed");
+                        }
+                    } else {
+                        return JSONResponses.error("Missing public key"); // shouldn't happen
+                    }
+                }
+            } else {
+                data = prunableMessage.getMessage();
+            }
         }
-        byte[] data = prunableMessage.getMessage();
+        if (data == null) {
+            data = Convert.EMPTY_BYTE;
+        }
         response.setHeader("Content-Disposition", "inline; filename=" + Long.toUnsignedString(transactionId));
         response.setContentLength(data.length);
         try (OutputStream out = response.getOutputStream()) {
