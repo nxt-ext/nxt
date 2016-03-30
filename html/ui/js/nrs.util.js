3635fddd38cc7245ee798ef387c9aa2e18d3bd80
lyaffe <lyaffe@gmail.com>
2016-03-30 19:31:22 +0300

    Enable update check on testnet and refactoring of the nrs.update.js code

--- html/ui/js/nrs.util.js
+++ html/ui/js/nrs.util.js
@@ -1702,5 +1702,70 @@ var NRS = (function (NRS, $, undefined) {
         return buf;
     };
 
+    NRS.versionCompare = function (v1, v2) {
+        if (v2 == undefined) {
+            return -1;
+        } else if (v1 == undefined) {
+            return -1;
+        }
+
+        //https://gist.github.com/TheDistantSea/8021359 (based on)
+        var v1last = v1.slice(-1);
+        var v2last = v2.slice(-1);
+
+        if (v1last == 'e') {
+            v1 = v1.substring(0, v1.length - 1);
+        } else {
+            v1last = '';
+        }
+
+        if (v2last == 'e') {
+            v2 = v2.substring(0, v2.length - 1);
+        } else {
+            v2last = '';
+        }
+
+        var v1parts = v1.split('.');
+        var v2parts = v2.split('.');
+
+        function isValidPart(x) {
+            return /^\d+$/.test(x);
+        }
+
+        if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+            return NaN;
+        }
+
+        v1parts = v1parts.map(Number);
+        v2parts = v2parts.map(Number);
+
+        for (var i = 0; i < v1parts.length; ++i) {
+            if (v2parts.length == i) {
+                return 1;
+            }
+            if (v1parts[i] != v2parts[i]) {
+                if (v1parts[i] > v2parts[i]) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        }
+
+        if (v1parts.length != v2parts.length) {
+            return -1;
+        }
+
+        if (v1last && v2last) {
+            return 0;
+        } else if (v1last) {
+            return 1;
+        } else if (v2last) {
+            return -1;
+        } else {
+            return 0;
+        }
+    };
+
     return NRS;
 }(NRS || {}, jQuery));
