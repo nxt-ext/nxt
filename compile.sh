8acfe75888526439e045cec2ed181b254ec4babd
Jean-Luc Picard <jlp@mailbox.org>
2016-04-05 18:21:37 +0300

    also include addon paths when compiling them

--- compile.sh
+++ compile.sh
@@ -17,8 +17,7 @@ echo "nxt core class files compiled successfully"
 echo "compiling nxt desktop..."
 find src/java/nxtdesktop/ -name "*.java" > sources.tmp
 javac -encoding utf8 -sourcepath "${SP}" -classpath "${CP}" -d classes/ @sources.tmp
-if [ $? -eq 0 ];
-then
+if [ $? -eq 0 ]; then
     echo "nxt desktop class files compiled successfully"
 else
     echo "if javafx is not supported, nxt desktop compile errors are safe to ignore, but desktop wallet will not be available"
@@ -27,10 +26,9 @@ fi
 rm -f sources.tmp
 
 find addons/src/ -name "*.java" > addons.tmp
-if [ -s addons.tmp ]
-then
+if [ -s addons.tmp ]; then
     echo "compiling add-ons..."
-    javac -encoding utf8 -sourcepath "${SP}" -classpath "${CP}" -d addons/classes @addons.tmp || exit 1
+    javac -encoding utf8 -sourcepath "${SP}:addons/src" -classpath "${CP}:addons/classes" -d addons/classes @addons.tmp || exit 1
     echo "add-ons compiled successfully"
     rm -f addons.tmp
 else
