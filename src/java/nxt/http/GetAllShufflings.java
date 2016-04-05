13f0def8656f95242aa3ea3fae1e3f74a43251bc
Jean-Luc Picard <jlp@mailbox.org>
2016-04-05 15:32:15 +0300

    added finishedOnly parameter to getPolls and getAllShufflings

--- src/java/nxt/http/GetAllShufflings.java
+++ src/java/nxt/http/GetAllShufflings.java
@@ -18,6 +18,7 @@ package nxt.http;
 
 import nxt.Shuffling;
 import nxt.db.DbIterator;
+import nxt.db.DbUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONStreamAware;
@@ -29,13 +30,14 @@ public final class GetAllShufflings extends APIServlet.APIRequestHandler {
     static final GetAllShufflings instance = new GetAllShufflings();
 
     private GetAllShufflings() {
-        super(new APITag[] {APITag.SHUFFLING}, "includeFinished", "includeHoldingInfo", "firstIndex", "lastIndex");
+        super(new APITag[] {APITag.SHUFFLING}, "includeFinished", "includeHoldingInfo", "finishedOnly", "firstIndex", "lastIndex");
     }
 
     @Override
     protected JSONStreamAware processRequest(HttpServletRequest req) {
 
         boolean includeFinished = "true".equalsIgnoreCase(req.getParameter("includeFinished"));
+        boolean finishedOnly = "true".equalsIgnoreCase(req.getParameter("finishedOnly"));
         boolean includeHoldingInfo = "true".equalsIgnoreCase(req.getParameter("includeHoldingInfo"));
         int firstIndex = ParameterParser.getFirstIndex(req);
         int lastIndex = ParameterParser.getLastIndex(req);
@@ -43,11 +45,20 @@ public final class GetAllShufflings extends APIServlet.APIRequestHandler {
         JSONObject response = new JSONObject();
         JSONArray jsonArray = new JSONArray();
         response.put("shufflings", jsonArray);
-        try (DbIterator<Shuffling> shufflings = includeFinished ?
-                Shuffling.getAll(firstIndex, lastIndex) : Shuffling.getActiveShufflings(firstIndex, lastIndex)) {
+        DbIterator<Shuffling> shufflings = null;
+        try {
+            if (finishedOnly) {
+                shufflings = Shuffling.getFinishedShufflings(firstIndex, lastIndex);
+            } else if (includeFinished) {
+                shufflings = Shuffling.getAll(firstIndex, lastIndex);
+            } else {
+                shufflings = Shuffling.getActiveShufflings(firstIndex, lastIndex);
+            }
             for (Shuffling shuffling : shufflings) {
                 jsonArray.add(JSONData.shuffling(shuffling, includeHoldingInfo));
             }
+        } finally {
+            DbUtils.close(shufflings);
         }
         return response;
     }
