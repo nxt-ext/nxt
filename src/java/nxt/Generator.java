1952378fd03925f6c85b2ea43c994df2d87f746e
Jean-Luc Picard <jlp@mailbox.org>
2016-05-14 11:53:24 +0300

    do not start block generation and transaction processing threads when isLightClient

--- src/java/nxt/Generator.java
+++ src/java/nxt/Generator.java
@@ -113,7 +113,9 @@ public final class Generator implements Comparable<Generator> {
     };
 
     static {
-        ThreadPool.scheduleThread("GenerateBlocks", generateBlocksThread, 500, TimeUnit.MILLISECONDS);
+        if (!Constants.isLightClient) {
+            ThreadPool.scheduleThread("GenerateBlocks", generateBlocksThread, 500, TimeUnit.MILLISECONDS);
+        }
     }
 
     static void init() {}
--- src/java/nxt/TransactionProcessorImpl.java
+++ src/java/nxt/TransactionProcessorImpl.java
@@ -304,11 +304,13 @@ final class TransactionProcessorImpl implements TransactionProcessor {
 
 
     private TransactionProcessorImpl() {
-        ThreadPool.scheduleThread("ProcessTransactions", processTransactionsThread, 5);
-        ThreadPool.scheduleThread("RemoveUnconfirmedTransactions", removeUnconfirmedTransactionsThread, 20);
-        ThreadPool.scheduleThread("ProcessWaitingTransactions", processWaitingTransactionsThread, 1);
-        ThreadPool.runAfterStart(this::rebroadcastAllUnconfirmedTransactions);
-        ThreadPool.scheduleThread("RebroadcastTransactions", rebroadcastTransactionsThread, 23);
+        if (!Constants.isLightClient) {
+            ThreadPool.scheduleThread("ProcessTransactions", processTransactionsThread, 5);
+            ThreadPool.scheduleThread("RemoveUnconfirmedTransactions", removeUnconfirmedTransactionsThread, 20);
+            ThreadPool.scheduleThread("ProcessWaitingTransactions", processWaitingTransactionsThread, 1);
+            ThreadPool.runAfterStart(this::rebroadcastAllUnconfirmedTransactions);
+            ThreadPool.scheduleThread("RebroadcastTransactions", rebroadcastTransactionsThread, 23);
+        }
     }
 
     @Override
