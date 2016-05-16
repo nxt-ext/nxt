cdbb1ed3afc91b859cb7ef90f482ea0ea0d47ab3
lyaffe <lyaffe@gmail.com>
2016-05-16 23:17:19 +0300

    Fix #343 Batch files compact.bat, mint.bat not working under windows

--- mint.bat
+++ mint.bat
@@ -1,2 +1,6 @@
-@ECHO OFF
-java.exe -cp classes;lib\*;conf nxt.mint.MintWorker
+rem @ECHO OFF
+if exist jre ( 
+    set javaDir=jre\bin\
+)
+
+%javaDir%java.exe -cp classes;lib\*;conf nxt.mint.MintWorker
