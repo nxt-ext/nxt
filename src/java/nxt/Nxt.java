dba3ed4ae051b802cdef41a6a905f48b592255ed
Jean-Luc Picard <jlp@mailbox.org>
2016-06-20 01:09:09 +0300

    1.9.1e

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -50,7 +50,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "1.9.0e";
+    public static final String VERSION = "1.9.1e";
     public static final String APPLICATION = "NRS";
 
     private static volatile Time time = new Time.EpochTime();
