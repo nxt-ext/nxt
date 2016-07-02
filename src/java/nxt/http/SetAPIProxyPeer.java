ca93b84b87f1f43c70a1cdd9404b7d2f038844a5
Jean-Luc Picard <jlp@mailbox.org>
2016-07-03 01:23:56 +0300

    rename parameter for consistency

--- src/java/nxt/http/SetAPIProxyPeer.java
+++ src/java/nxt/http/SetAPIProxyPeer.java
@@ -5,24 +5,25 @@ import nxt.peer.Peer;
 import nxt.peer.Peers;
 import nxt.util.Convert;
 import nxt.util.JSON;
-import org.json.simple.JSONObject;
 import org.json.simple.JSONStreamAware;
 
 import javax.servlet.http.HttpServletRequest;
 
-import static nxt.http.JSONResponses.*;
+import static nxt.http.JSONResponses.PEER_NOT_CONNECTED;
+import static nxt.http.JSONResponses.PEER_NOT_OPEN_API;
+import static nxt.http.JSONResponses.UNKNOWN_PEER;
 
 public class SetAPIProxyPeer extends APIServlet.APIRequestHandler {
 
     static final SetAPIProxyPeer instance = new SetAPIProxyPeer();
 
     private SetAPIProxyPeer() {
-        super(new APITag[] {APITag.NETWORK}, "peerAddress");
+        super(new APITag[] {APITag.NETWORK}, "peer");
     }
 
     @Override
     protected JSONStreamAware processRequest(HttpServletRequest request) throws NxtException {
-        String peerAddress = Convert.emptyToNull(request.getParameter("peerAddress"));
+        String peerAddress = Convert.emptyToNull(request.getParameter("peer"));
         if (peerAddress == null) {
             APIProxy.getInstance().setForcedPeer(null);
             return JSON.emptyJSON;
