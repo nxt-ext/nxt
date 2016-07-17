680e9b9cffae06d4da800f5ee75bbc42bc314c0a
lyaffe <lyaffe@gmail.com>
2016-07-17 14:50:53 +0300

    Single modal now supports multiple request types using the button data-request attribute

--- html/ui/js/nrs.forms.js
+++ html/ui/js/nrs.forms.js
@@ -218,7 +218,12 @@ var NRS = (function(NRS, $) {
 			$form = $modal.find("form:first");
 		}
 
-		var requestType = $form.find("input[name=request_type]").val();
+		var requestType;
+		if ($btn.data("request")) {
+			requestType = $btn.data("request");
+		} else {
+			$form.find("input[name=request_type]").val();
+		}
 		var requestTypeKey = requestType.replace(/([A-Z])/g, function($1) {
 			return "_" + $1.toLowerCase();
 		});
