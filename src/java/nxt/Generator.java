819565dbc5e34925c89c1fbbc90f281038712d2e
ScripterRon <Ronald.Hoffman6@gmail.com>
2016-07-21 16:51:57 -0400

    Copyright mangled by IDE

--- src/java/nxt/Generator.java
+++ src/java/nxt/Generator.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright Ãƒâ€šÃ‚Â© 2013-2016 The Nxt Core Developers.                             *
+ * Copyright © 2013-2016 The Nxt Core Developers.                             *
  *                                                                            *
  * See the AUTHORS.txt, DEVELOPER-AGREEMENT.txt and LICENSE.txt files at      *
  * the top-level directory of this distribution for the individual copyright  *
