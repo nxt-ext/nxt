f824eb0ae8d7e80c105872bec6ec8dcaf43e0b3a
Lizyaffe <lyaffe@gmail.com>
2016-07-24 15:04:25 +0300

    #356 Ardor distribution status display

--- html/ui/js/nrs.header.js
+++ html/ui/js/nrs.header.js
@@ -69,13 +69,18 @@ var NRS = (function(NRS, $) {
             $("#ardor_distribution_end_time").html(NRS.getBlockHeightTimeEstimate(response.distributionEnd));
             $("#ardor_distribution_current_balance").html(NRS.formatQuantity(response.quantityQNT, 4));
             $("#ardor_distribution_expected_balance").html(NRS.formatQuantity(response.totalExpectedQuantityQNT, 4));
+            var now = moment();
+            var nextSnapshot = NRS.lastBlockHeight + response.distributionStep - ((NRS.lastBlockHeight - response.distributionStart) % response.distributionStep);
+            $("#ardor_distribution_next_snapshot").html(moment.duration(NRS.getBlockHeightMoment(nextSnapshot).diff(now)).humanize());
+            var nextUpdate = NRS.lastBlockHeight + response.distributionFrequency - ((NRS.lastBlockHeight - response.distributionStart) % response.distributionFrequency);
+            $("#ardor_distribution_next_balance_update").html(moment.duration(NRS.getBlockHeightMoment(nextUpdate).diff(now)).humanize());
 
             var duration;
             if (response.distributionStart > NRS.lastBlockHeight) {
-                duration = moment.duration(NRS.getBlockHeightMoment(response.distributionStart).diff(moment()));
+                duration = moment.duration(NRS.getBlockHeightMoment(response.distributionStart).diff(now));
                 $("#ardor_distribution_modal").find(".fomo_message").html($.t("distribution_starts_in", { interval: duration.humanize() }));
             } else {
-                duration = moment.duration(NRS.getBlockHeightMoment(response.distributionEnd).diff(moment()));
+                duration = moment.duration(NRS.getBlockHeightMoment(response.distributionEnd).diff(now));
                 $("#ardor_distribution_modal").find(".fomo_message").html($.t("distribution_ends_in", {interval: duration.humanize()}));
             }
         });
