84fcb6c77e3ab146c5a35d6c348a6441af33cd74
Lizyaffe <lyaffe@gmail.com>
2016-08-01 16:02:27 +0300

    Merge UI changed from feature/ui to make the lock screen lock better, hide the logo icon when typing account number and slightly better rendering on small screen.

--- html/www/js/nrs.login.js
+++ html/www/js/nrs.login.js
@@ -166,8 +166,7 @@ var NRS = (function(NRS, $, undefined) {
 					);
 				}
 			});
-			var otherHTML = "<li><a href='#'";
-			otherHTML += "data-i18n='other'><em>Add Another Account</em></a></li>";
+			var otherHTML = "<li><a href='#' data-i18n='other'>Other</a></li>";
 			var $otherHTML = $(otherHTML);
 			$otherHTML.click(function() {
 				$('#login_account_container').hide();
@@ -215,23 +214,23 @@ var NRS = (function(NRS, $, undefined) {
             $.growl($.t("switched_to_account", { account: account }))
         }, true);
 	};
-	
+
 	$("#loginButtons").on('click',function(e) {
 		e.preventDefault();
 		if ($(this).data( "login-type" ) == "password") {
-      NRS.listAccounts();
+			NRS.listAccounts();
 			$('#login_password').parent().hide();
 			$('#remem-wrap').addClass('remem-pass-hide');
 			$(this).html('<input type="hidden" name="loginType" id="accountLogin" value="account" autocomplete="off" /><i class="fa fa-male"></i>');
 			$(this).data( "login-type","account");
-    } else {
-      $('#login_account_container').hide();
+		} else {
+			$('#login_account_container').hide();
 			$('#login_account_container_other').hide();
 			$('#login_password').parent().show();
 			$('#remem-wrap').removeClass('remem-pass-hide');
 			$(this).html('<input type="hidden" name="loginType" id="accountLogin" value="passwordLogin" autocomplete="off" /><i class="fa fa-key"></i>');
 			$(this).data( "login-type","password");
-        }
+		}
 	});
 	
 	NRS.removeAccount = function(account) {
