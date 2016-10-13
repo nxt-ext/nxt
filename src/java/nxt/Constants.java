6254c7fa891a174440ee3e47b45ad61304947052
Jean-Luc Picard <jlp@mailbox.org>
2016-10-13 18:07:57 +0300

    checkpoint after FXT_BLOCK

--- src/java/nxt/Constants.java
+++ src/java/nxt/Constants.java
@@ -151,9 +151,10 @@ public final class Constants {
     public static final int CHECKSUM_BLOCK_18 = isTestnet ? 664000 : 729700;
     public static final int CHECKSUM_BLOCK_19 = isTestnet ? 789000 : 851000;
     public static final int FXT_BLOCK = isTestnet ? 779000 : 1000000;
+    public static final int CHECKSUM_BLOCK_20 = isTestnet ? 944000 : 1000100;
 
-    public static final int LAST_CHECKSUM_BLOCK = CHECKSUM_BLOCK_19;
-    public static final int LAST_KNOWN_BLOCK = isTestnet ? 847000 : 907000;
+    public static final int LAST_CHECKSUM_BLOCK = CHECKSUM_BLOCK_20;
+    public static final int LAST_KNOWN_BLOCK = CHECKSUM_BLOCK_20;
 
     public static final int[] MIN_VERSION = new int[] {1, 10, 1};
     public static final int[] MIN_PROXY_VERSION = new int[] {1, 10, 1};
