f6386e8c1779fea1f5c4f73b8c17fb2f827d19a9
lyaffe <lyaffe@gmail.com>
2016-10-23 12:33:09 +0300

    Change res folder to icons

--- mobile/app/createapp.bat
+++ mobile/app/createapp.bat
@@ -1,7 +1,7 @@
 rmdir /s /q wallet
 call cordova create wallet org.nxt.mobile.wallet "NXT Mobile Wallet" --template ..\..\html
 cd wallet
-xcopy /y/i/s ..\..\res res
+xcopy /y/i/s ..\..\icons icons
 call cordova platform add android
 xcopy /y/i/s ..\..\platforms platforms
 call cordova plugin add cordova-plugin-file
