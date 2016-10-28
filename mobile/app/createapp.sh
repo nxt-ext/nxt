3cb7b887ae0234276fe83e5084e0cb46aef34e6d
lyaffe <lyaffe@gmail.com>
2016-10-28 18:08:48 +0300

    Parameterize the platform on Unix script to allow creating an iOS app

--- mobile/app/createapp.sh
+++ mobile/app/createapp.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+PLATFORM=$1
+if [ -x ${PLATFORM} ];
+then
+	echo PLATFORM not defined
+	exit 1
+fi
 rm -rf wallet
 cordova create wallet org.nxt.mobile.wallet "NXT Mobile Wallet" --template ../../html
 cd wallet
@@ -6,7 +12,7 @@ rm -rf icons
 rm -rf plugins
 cp -a ../../icons icons
 cp -a ../../plugins plugins
-cordova platform add android
+cordova platform add ${PLATFORM}
 rm -rf platforms
 cp -a ../../platforms platforms
 cd ..
--- mobile/app/runapp.sh
+++ mobile/app/runapp.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+PLATFORM=$1
+if [ -x ${PLATFORM} ];
+then
+	echo PLATFORM not defined
+	exit 1
+fi
 ./updateapp.sh
 cd wallet
-cordova run android
+cordova run ${PLATFORM}
 cd ..
