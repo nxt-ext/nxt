89a89afa3c16e04350198bf0d2e4ef902504cd2e
lyaffe <lyaffe@gmail.com>
2017-01-02 15:22:31 +0200

    Support both development and release modes for the samples

--- html/www/js/sample/send.money.js
+++ html/www/js/sample/send.money.js
@@ -2,7 +2,14 @@ const url = "http://mclyaf03:6876";
 const secretPhrase = "hope peace happen touch easy pretend worthless talk them indeed wheel state";
 const recipientPublicKey = "0b4e505972149e7ceb51309edc76729795cabe1f2cc42d87688138d0966db436";
 
-var bridge = require("./../nrs.node.bridge.js").init({
+var bridge = require("./../nrs.node.bridge.js"); // during development
+try {
+    bridge = require("nxt-blockchain"); // when using the NPM module
+} catch(e) {
+    console.log(e.message);
+}
+
+bridge.init({
     url: url,
     secretPhrase: secretPhrase,
     isTestNet: true
