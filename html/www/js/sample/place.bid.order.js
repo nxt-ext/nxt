4ae4ba499090e3a261a79018ebb1bba2296adb45
lyaffe <lyaffe@gmail.com>
2017-01-03 17:03:02 +0200

    Sample for bid and ask orders

---
+++ html/www/js/sample/place.bid.order.js
@@ -0,0 +1,39 @@
+const url = "http://mclyaf03:6876";
+const secretPhrase = "hope peace happen touch easy pretend worthless talk them indeed wheel state";
+
+try {
+    var bridge = require("./../nrs.node.bridge.js"); // during development
+} catch(e) {
+    console.log("Release mode");
+}
+
+try {
+    bridge = require("nxt-blockchain"); // when using the NPM module
+} catch(e) {
+    console.log("Development mode");
+}
+
+bridge.init({
+    url: url,
+    secretPhrase: secretPhrase,
+    isTestNet: true
+});
+
+bridge.load(function(NRS) {
+    const decimals = 2;
+    var quantity = 123.45;
+    var price = 1.2;
+    var data = {
+        asset: "6094526212840718212",
+        quantityQNT: NRS.convertToQNT(quantity, decimals),
+        priceNQT: NRS.calculatePricePerWholeQNT(NRS.convertToNQT(price), decimals),
+        secretPhrase: secretPhrase
+    };
+    data = Object.assign(
+        data,
+        NRS.getMandatoryParams()
+    );
+    NRS.sendRequest("placeBidOrder", data, function (response) {
+        NRS.logConsole(JSON.stringify(response));
+    });
+});
