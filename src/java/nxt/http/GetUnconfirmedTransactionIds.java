46017d84650ead1de67888dd37d497c6474f87a1
Jean-Luc Picard <jlp@mailbox.org>
2017-03-17 21:57:05 +0200

    added pagination to getUnconfirmedTransactions and getUnconfirmedTransactionIds

--- src/java/nxt/http/GetUnconfirmedTransactionIds.java
+++ src/java/nxt/http/GetUnconfirmedTransactionIds.java
@@ -19,8 +19,8 @@ package nxt.http;
 import nxt.Nxt;
 import nxt.Transaction;
 import nxt.db.DbIterator;
+import nxt.db.FilteringIterator;
 import nxt.util.Convert;
-import nxt.util.Filter;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONStreamAware;
@@ -33,21 +33,31 @@ public final class GetUnconfirmedTransactionIds extends APIServlet.APIRequestHan
     static final GetUnconfirmedTransactionIds instance = new GetUnconfirmedTransactionIds();
 
     private GetUnconfirmedTransactionIds() {
-        super(new APITag[] {APITag.TRANSACTIONS, APITag.ACCOUNTS}, "account", "account", "account");
+        super(new APITag[] {APITag.TRANSACTIONS, APITag.ACCOUNTS}, "account", "account", "account", "firstIndex", "lastIndex");
     }
 
     @Override
     protected JSONStreamAware processRequest(HttpServletRequest req) throws ParameterException {
 
         Set<Long> accountIds = Convert.toSet(ParameterParser.getAccountIds(req, false));
-        Filter<Transaction> filter = accountIds.isEmpty() ? transaction -> true :
-                transaction -> accountIds.contains(transaction.getSenderId()) || accountIds.contains(transaction.getRecipientId());
+        int firstIndex = ParameterParser.getFirstIndex(req);
+        int lastIndex = ParameterParser.getLastIndex(req);
 
         JSONArray transactionIds = new JSONArray();
-        try (DbIterator<? extends Transaction> transactionsIterator = Nxt.getTransactionProcessor().getAllUnconfirmedTransactions()) {
-            while (transactionsIterator.hasNext()) {
-                Transaction transaction = transactionsIterator.next();
-                if (filter.ok(transaction)) {
+        if (accountIds.isEmpty()) {
+            try (DbIterator<? extends Transaction> transactionsIterator = Nxt.getTransactionProcessor().getAllUnconfirmedTransactions(firstIndex, lastIndex)) {
+                while (transactionsIterator.hasNext()) {
+                    Transaction transaction = transactionsIterator.next();
+                    transactionIds.add(transaction.getStringId());
+                }
+            }
+        } else {
+            try (FilteringIterator<? extends Transaction> transactionsIterator = new FilteringIterator<> (
+                    Nxt.getTransactionProcessor().getAllUnconfirmedTransactions(0, -1),
+                    transaction -> accountIds.contains(transaction.getSenderId()) || accountIds.contains(transaction.getRecipientId()),
+                    firstIndex, lastIndex)) {
+                while (transactionsIterator.hasNext()) {
+                    Transaction transaction = transactionsIterator.next();
                     transactionIds.add(transaction.getStringId());
                 }
             }
