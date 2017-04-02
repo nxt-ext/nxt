356581e6f013a4556d23a1590fdd59f38e699f01
lyaffe <lyaffe@gmail.com>
2017-04-02 12:32:58 +0300

    Script to update Cordova plugins

---
+++ mobile/app/updateplugins.bat
@@ -0,0 +1,14 @@
+pushd wallet
+call cordova plugin add cordova-plugin-android-permissions --save
+call cordova plugin add cordova-plugin-compat --save
+call cordova plugin add cordova-plugin-console --save
+call cordova plugin add cordova-plugin-device --save
+call cordova plugin add cordova-plugin-file --save
+call cordova plugin add cordova-plugin-inappbrowser --save
+call cordova plugin add cordova-plugin-splashscreen --save
+call cordova plugin add cordova-plugin-statusbar --save
+call cordova plugin add cordova-plugin-whitelist --save
+call cordova plugin add cordova.plugins.diagnostic --save
+call cordova plugin add ionic-plugin-keyboard --save
+call cordova plugin add phonegap-plugin-barcodescanner --save
+popd
