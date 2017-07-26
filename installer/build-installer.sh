c2e633932e6ac7698804932f1651d81ea7aa98cf
Tomislav Gountchev <tomislav.gountchev@jelurida.com>
2017-07-26 22:13:08 +0300

    fix build-installer.sh to work with cygwin

--- installer/build-installer.sh
+++ installer/build-installer.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+PATHSEP=":"
+if [ "$OSTYPE" = "cygwin" ] ; then
+PATHSEP=";"
+fi
+
 CLASSDIR=../installer/panels/classes
 CONFDIR=$CLASSDIR/nxt/installer/resources
 JAR=$CLASSDIR/nxt-panels.jar
@@ -16,7 +21,7 @@ cp ../conf/examples/*.properties $CONFDIR
 jar cf0 $JAR -C $CLASSDIR nxt
 
 # package the installer
-java -Xmx512m -cp "../installer/lib/*:$JAR" com.izforge.izpack.compiler.bootstrap.CompilerLauncher ../installer/setup.xml -o $1.jar > ../installer/build-installer.log 2>&1
+java -Xmx512m -cp "../installer/lib/*${PATHSEP}${JAR}" com.izforge.izpack.compiler.bootstrap.CompilerLauncher ../installer/setup.xml -o $1.jar > ../installer/build-installer.log 2>&1
 
 # cleanup
 rm -rf $CLASSDIR
