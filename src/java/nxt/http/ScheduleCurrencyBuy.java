73e9ea883a04b74d94a0399e6fe99436e9f75ad6
Tomislav Gountchev <tomislav.gountchev@jelurida.com>
2017-08-07 10:53:06 +0300

    support submitting client-side prepared transaction json or bytes

--- src/java/nxt/http/ScheduleCurrencyBuy.java
+++ src/java/nxt/http/ScheduleCurrencyBuy.java
@@ -25,6 +25,7 @@ import nxt.NxtException;
 import nxt.Transaction;
 import nxt.TransactionScheduler;
 import nxt.db.DbIterator;
+import nxt.util.Convert;
 import nxt.util.Filter;
 import nxt.util.JSON;
 import org.json.simple.JSONObject;
@@ -38,52 +39,73 @@ public final class ScheduleCurrencyBuy extends CreateTransaction {
     static final ScheduleCurrencyBuy instance = new ScheduleCurrencyBuy();
 
     private ScheduleCurrencyBuy() {
-        super(new APITag[] {APITag.MS, APITag.CREATE_TRANSACTION}, "currency", "rateNQT", "units", "offerIssuer");
+        super(new APITag[] {APITag.MS, APITag.CREATE_TRANSACTION}, "currency", "rateNQT", "units", "offerIssuer",
+                "transactionJSON", "transactionBytes", "prunableAttachmentJSON");
     }
 
     @Override
     protected JSONStreamAware processRequest(HttpServletRequest req) throws NxtException {
-        boolean broadcast = !"false".equalsIgnoreCase(req.getParameter("broadcast"));
-        if (broadcast) {
-            return JSONResponses.error("Must use broadcast=false to schedule a future currency buy");
-        }
-        Currency currency = ParameterParser.getCurrency(req);
-        long rateNQT = ParameterParser.getLong(req, "rateNQT", 0, Long.MAX_VALUE, true);
-        long units = ParameterParser.getLong(req, "units", 0, Long.MAX_VALUE, true);
-        Account account = ParameterParser.getSenderAccount(req);
-        String secretPhrase = ParameterParser.getSecretPhrase(req, false);
-
+        String transactionJSON = Convert.emptyToNull(req.getParameter("transactionJSON"));
+        String transactionBytes = Convert.emptyToNull(req.getParameter("transactionBytes"));
+        String prunableAttachmentJSON = Convert.emptyToNull(req.getParameter("prunableAttachmentJSON"));
         long offerIssuerId = ParameterParser.getAccountId(req, "offerIssuer", true);
-        Filter<Transaction> filter = new ExchangeOfferFilter(offerIssuerId, currency.getId(), rateNQT);
 
-        Attachment attachment = new Attachment.MonetarySystemExchangeBuy(currency.getId(), rateNQT, units);
         try {
-            JSONObject json = (JSONObject)JSONValue.parse(JSON.toString(createTransaction(req, account, attachment)));
-            if (secretPhrase != null) {
-                JSONObject transactionJSON = (JSONObject) json.get("transactionJSON");
-                Transaction.Builder builder = Nxt.newTransactionBuilder(transactionJSON);
-                Transaction transaction = builder.build();
-                Nxt.getBlockchain().readLock();
+            JSONObject response;
+            Transaction transaction;
+            if (transactionBytes == null && transactionJSON == null) {
+                boolean broadcast = !"false".equalsIgnoreCase(req.getParameter("broadcast"));
+                if (broadcast) {
+                    return JSONResponses.error("Must use broadcast=false to schedule a future currency buy");
+                }
+                Currency currency = ParameterParser.getCurrency(req);
+                long rateNQT = ParameterParser.getLong(req, "rateNQT", 0, Long.MAX_VALUE, true);
+                long units = ParameterParser.getLong(req, "units", 0, Long.MAX_VALUE, true);
+                Account account = ParameterParser.getSenderAccount(req);
+                String secretPhrase = ParameterParser.getSecretPhrase(req, false);
+                Attachment attachment = new Attachment.MonetarySystemExchangeBuy(currency.getId(), rateNQT, units);
+                response = (JSONObject)JSONValue.parse(JSON.toString(createTransaction(req, account, attachment)));
+                if (secretPhrase == null) {
+                    response.put("scheduled", false);
+                    return response;
+                }
+                transaction = Nxt.newTransactionBuilder((JSONObject) response.get("transactionJSON")).build();
+            } else {
+                response = new JSONObject();
+                transaction = ParameterParser.parseTransaction(transactionJSON, transactionBytes, prunableAttachmentJSON).build();
+                JSONObject json = JSONData.unconfirmedTransaction(transaction);
+                response.put("transactionJSON", json);
                 try {
-                    transaction.validate();
-                    try (DbIterator<? extends Transaction> unconfirmedTransactions = Nxt.getTransactionProcessor().getAllUnconfirmedTransactions()) {
-                        while (unconfirmedTransactions.hasNext()) {
-                            if (filter.ok(unconfirmedTransactions.next())) {
-                                Nxt.getTransactionProcessor().broadcast(transaction);
-                                json.put("broadcasted", true);
-                                return json;
-                            }
+                    response.put("unsignedTransactionBytes", Convert.toHexString(transaction.getUnsignedBytes()));
+                } catch (NxtException.NotYetEncryptedException ignore) {}
+                response.put("transactionBytes", Convert.toHexString(transaction.getBytes()));
+                response.put("signatureHash", json.get("signatureHash"));
+                response.put("transaction", transaction.getStringId());
+                response.put("fullHash", transaction.getFullHash());
+            }
+
+            Attachment.MonetarySystemExchangeBuy attachment = (Attachment.MonetarySystemExchangeBuy)transaction.getAttachment();
+            Filter<Transaction> filter = new ExchangeOfferFilter(offerIssuerId, attachment.getCurrencyId(), attachment.getRateNQT());
+
+            Nxt.getBlockchain().readLock();
+            try {
+                transaction.validate();
+                try (DbIterator<? extends Transaction> unconfirmedTransactions = Nxt.getTransactionProcessor().getAllUnconfirmedTransactions()) {
+                    while (unconfirmedTransactions.hasNext()) {
+                        if (filter.ok(unconfirmedTransactions.next())) {
+                            Nxt.getTransactionProcessor().broadcast(transaction);
+                            response.put("broadcasted", true);
+                            return response;
                         }
                     }
-                    TransactionScheduler.schedule(filter, transaction);
-                    json.put("scheduled", true);
-                } finally {
-                    Nxt.getBlockchain().readUnlock();
                 }
-            } else {
-                json.put("scheduled", false);
+                TransactionScheduler.schedule(filter, transaction);
+                response.put("scheduled", true);
+            } finally {
+                Nxt.getBlockchain().readUnlock();
             }
-            return json;
+            return response;
+
         } catch (NxtException.InsufficientBalanceException e) {
             return JSONResponses.NOT_ENOUGH_FUNDS;
         }
